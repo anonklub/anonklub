3a5d1d42b086d15ce6e32f666ebe12fe6d0084c1
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-13 15:41:41 +0100

    :arrow_up: 🐛 Bump `duneapi` python dep

--- apis/query/src/lib/dune/query.py
+++ apis/query/src/lib/dune/query.py
@@ -3,31 +3,24 @@ import os
 import sys
 
 from duneapi.api import DuneAPI
-from duneapi.types import QueryParameter
+from duneapi.types import DashboardTile, DuneQuery
 
-QUERY_PARAM_TYPES = {
-    'enum': QueryParameter.enum_type,
-    'date': QueryParameter.date_type,
-    'number': QueryParameter.number_type,
-    'text': QueryParameter.text_type,
-}
-
-
-def parse_parameters(_parameters: str):
-    return [QUERY_PARAM_TYPES[p['type']](p['name'], p['value']) for p in json.loads(_parameters)]
-
-
-dune = DuneAPI(os.environ['DUNE_USER'], os.environ['DUNE_PWD'])
+dune = DuneAPI(os.environ["DUNE_USER"], os.environ["DUNE_PWD"])
 dune.login()
 
 query_id = int(sys.argv[1])
+parameters = None
 
 # [{\"name\":\"min\",\"type\":\"number\",\"value\":10},{\"name\":\"tokenAddress\",\"type\":\"text\",\"value\":\"'0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'\"}]
-parameters = None
 if len(sys.argv[1:]) == 2:
-    parameters = parse_parameters(sys.argv[2])
+    parameters = json.loads(sys.argv[2])
 
-job_id = dune.execute(query_id, parameters)
-records = dune.get_results(job_id)
+# duneapi api is a bit weird, can't simply pass a query id and execute, need to provide a query_file parameter even
+# if unused
+dashboard_tile = DashboardTile.from_dict(
+    {"id": query_id, "parameters": parameters, "query_file": "./.empty", "network": "mainnet"}, os.getcwd()
+)
+dune_query = DuneQuery.from_tile(dashboard_tile)
+records = dune.execute_and_await_results(dune_query)
 
-print(json.dumps([r['address'] for r in records]))
+print(json.dumps([r["address"] for r in records]))
--- queries/dune_analytics/main.py
+++ queries/dune_analytics/main.py
@@ -1,23 +1,26 @@
+import json
 import os
 
 from duneapi.api import DuneAPI
-from duneapi.types import QueryParameter
+from duneapi.types import DuneQuery, DashboardTile
 
-queries = {
-    'beacon': 1499468,
-    'tornado': 1499520,
-    'erc20_balance': 1500107
-}
+queries = {"beacon": 1499468, "tornado": 1499520, "erc20_balance": 1500107}
 
-dune_api = DuneAPI(os.environ['DUNE_USER'], os.environ['DUNE_PWD'])
+dune_api = DuneAPI(os.environ["DUNE_USER"], os.environ["DUNE_PWD"])
 dune_api.login()
 
-parameters = [QueryParameter.number_type('min', 10),
-              QueryParameter.text_type('tokenAddress',
-                                       "'0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'")]
+parameters = [
+    {"key": "min", "type": "number", "value": 10},
+    {"key": "tokenAddress", "type": "text", "value": "'0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'"},
+]
+query_id = queries["erc20_balance"]
 
+# duneapi api is a bit weird, can't simply pass a query id and execute, need to provide a query_file parameter even
+# if unused
+dashboard_tile = DashboardTile.from_dict(
+    {"id": query_id, "parameters": parameters, "query_file": "./empty", "network": "mainnet"}, "."
+)
+dune_query = DuneQuery.from_tile(dashboard_tile)
+records = dune_api.execute_and_await_results(dune_query)
 
-job_id = dune_api.execute(queries['erc20_balance'], parameters)
-records = dune_api.get_results(job_id)
-
-print([r['address'] for r in records])
+print(json.dumps([r["address"] for r in records]))
