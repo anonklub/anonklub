ece38a20368aa8207e5385155a75fa587cb369ec
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-17 13:16:15 +0100

    :bug: Return empty array if subgraphs down or no results

--- apis/query/src/api/repositories/GraphRepository.ts
+++ apis/query/src/api/repositories/GraphRepository.ts
@@ -29,16 +29,16 @@ export class GraphRepository {
         id,
       },
     )
-    return data.proposal.votes.map((vote: Vote) => vote.voter.id)
+    return (data?.proposal ?? []).votes.map((vote: Vote) => vote.voter.id)
   }
 
   async getPunkOwners(): Promise<string[]> {
     const { data } = await execute(PunkOwnersDocument)
-    return data.punks.map((punk: Punk) => punk.owner.id)
+    return (data?.punks ?? []).map((punk: Punk) => punk.owner.id)
   }
 
   async getBeaconDepositors() {
     const { data } = await execute(BeaconDepositorsDocument)
-    return data.depositors.map((depositor: Depositor) => depositor.id)
+    return (data?.depositors ?? []).map((depositor: Depositor) => depositor.id)
   }
 }
