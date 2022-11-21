6d5405437a6d06ce025b11d683dcfb81252b4fd4
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-21 10:08:30 +0100

    :bug: Look for `.js` controllers/middlewares files in 'hosted' mode

--- apis/query/src/config/index.ts
+++ apis/query/src/config/index.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 
 const BASE_FOLDER = path.join(__dirname, '..', 'api')
-const ext = process.env.NODE_ENV === 'production' ? 'js' : 'ts'
+const ext = process.env.NODE_ENV === 'development' ? 'ts' : 'js'
 
 export const controllers = [path.join(BASE_FOLDER, 'controllers', `*.${ext}`)]
 export const middlewares = [path.join(BASE_FOLDER, 'middlewares', `*.${ext}`)]
