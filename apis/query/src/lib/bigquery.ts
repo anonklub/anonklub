30eaad28a9b8c7792b0fe999ada1ea3c10641855
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-21 11:00:36 +0100

    :mute: Don't show error stack in production (#41)
    * :mute: Don't show error stack in production
    
    * fix test

--- apis/query/src/lib/bigquery.ts
+++ apis/query/src/lib/bigquery.ts
@@ -10,21 +10,18 @@ export class Db extends BigQuery {
       NODE_ENV,
     } = process.env
 
-    if (NODE_ENV !== 'test') {
+    if (NODE_ENV === 'test') {
+      super({
+        credentials: {},
+        projectId: '',
+      })
+    } else {
       if (GOOGLE_APPLICATION_CREDENTIALS === undefined)
         throw new Error('missing google credentials')
       if (projectId === undefined) throw new Error('missing google project id')
-    }
 
-    if (process.env.NODE_ENV === 'hosted') {
       super({
-        // @ts-expect-error
         credentials: JSON.parse(GOOGLE_APPLICATION_CREDENTIALS),
-        projectId,
-      })
-    } else {
-      super({
-        keyFilename: GOOGLE_APPLICATION_CREDENTIALS,
         projectId,
       })
     }
