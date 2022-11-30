66566961cf2113727888835b5cc97fb46adafc25
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-30 12:32:03 +0100

    Add coveralls GH action

--- apis/query/jest.config.ts
+++ apis/query/jest.config.ts
@@ -4,6 +4,17 @@ import { pathsToModuleNameMapper } from 'ts-jest'
 import { compilerOptions } from './tsconfig.json'
 
 const jestConfig: JestConfigWithTsJest = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['index.ts', '/node_modules/'],
+  coverageThreshold: {
+    global: {
+      // branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
   moduleDirectories: ['node_modules', __dirname],
   moduleFileExtensions: ['ts', 'js', 'json'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
