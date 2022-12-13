c0f7a3389dcaf92a855c488e49ef77edebf3ef72
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-13 12:28:22 +0100

    update shell script

--- circuits/circom/maybe-fix-circom-imports.sh
+++ circuits/circom/maybe-fix-circom-imports.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-dest="$PWD/node_modules/circom-ecdsa/node_modules"
+dest="$PWD/node_modules/circom-ecdsa/node_modules/circomlib"
 
 if [[ ! -d $dir ]];then
-  src=node_modules/circomlib
+  mkdir -p $dest
+  src=node_modules/circomlib/circuits
   cp -r $src $dest
 fi
