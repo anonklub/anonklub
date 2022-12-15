30d64dc44b0ad834023d06c938b7d541fc7db058
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-15 13:52:08 +0100

    chore: Lint/Format

--- circuits/circom/test/circuit.test.ts
+++ circuits/circom/test/circuit.test.ts
@@ -1,15 +1,20 @@
-import { join } from 'path'
+import { Point, sign } from '@noble/secp256k1'
 import { wasm as wasm_tester } from 'circom_tester'
-import { BigNumber, utils } from 'ethers'
 import { buildPoseidon } from 'circomlibjs'
-import { sign, Point } from '@noble/secp256k1'
+import { BigNumber, utils } from 'ethers'
+import { Scalar } from 'ffjavascript'
 
-import { F1Field, Scalar } from 'ffjavascript'
-import { bigint_to_array, bigint_to_Uint8Array, Uint8Array_to_bigint } from './helpers'
+import { join } from 'path'
+import {
+  bigintToArray,
+  bigintToUint8Array,
+  uint8ArrayToBigint,
+} from './helpers'
 
 // FIXME: unused?
-exports.p = Scalar.fromString('21888242871839275222246405745257275088548364400416034343698204186575808495617')
-const Fr = new F1Field(exports.p)
+exports.p = Scalar.fromString(
+  '21888242871839275222246405745257275088548364400416034343698204186575808495617',
+)
 
 describe('Poseidon Merkle Tree', function () {
   let poseidon
@@ -19,14 +24,14 @@ describe('Poseidon Merkle Tree', function () {
   this.timeout(1000000)
 
   before(async () => {
-    let p = join(__dirname, 'merkle_tree_test.circom')
-    circuit = await wasm_tester(p,
+    const p = join(__dirname, 'merkle_tree_test.circom')
+    circuit = await wasm_tester(
+      p,
       // { "verbose": true }
     )
     poseidon = await buildPoseidon()
     F = poseidon.F // TODO: do we actually need this or is it the default field?
   })
-
 
   it('Should check membership in a depth 2 merkle tree', async () => {
     // merkle
@@ -36,11 +41,13 @@ describe('Poseidon Merkle Tree', function () {
     const indices = [1, 0]
 
     console.log(circuit.calculateWitness[0])
-    const w = await circuit.calculateWitness({leaf: leaf, root: root, pathElements: path, pathIndices: indices}, true)
+    const w = await circuit.calculateWitness(
+      { leaf, pathElements: path, pathIndices: indices, root },
+      true,
+    )
     await circuit.checkConstraints(w)
   })
 })
-
 
 describe('SetMembership', function () {
   this.timeout(1000 * 1000)
@@ -49,53 +56,62 @@ describe('SetMembership', function () {
 
   let circuit: any
   before(async function () {
-    circuit = await wasm_tester(join(__dirname, 'membership_test.circom'), {'verbose': true})
+    circuit = await wasm_tester(join(__dirname, 'membership_test.circom'), {
+      verbose: true,
+    })
     console.log('compiled circom')
     poseidon = await buildPoseidon()
     F = poseidon.F // TODO: do we actually need this or is it the default field?
   })
 
   it('Should produce valid proofs', async () => {
-    const privkeys: Array<bigint> = [88549154299169935420064281163296845505587953610183896504176354567359434168161n,
-                                     37706893564732085918706190942542566344879680306879183356840008504374628845468n,
-                                     90388020393783788847120091912026443124559466591761394939671630294477859800601n,
-                                     110977009687373213104962226057480551605828725303063265716157300460694423838923n]
+    const privkeys: bigint[] = [
+      88549154299169935420064281163296845505587953610183896504176354567359434168161n,
+      37706893564732085918706190942542566344879680306879183356840008504374628845468n,
+      90388020393783788847120091912026443124559466591761394939671630294477859800601n,
+      110977009687373213104962226057480551605828725303063265716157300460694423838923n,
+    ]
 
-    const addresses = privkeys.map(priv =>
-      BigNumber.from(utils.computeAddress(BigNumber.from(priv).toHexString())).toBigInt()
+    const addresses = privkeys.map((priv) =>
+      BigNumber.from(
+        utils.computeAddress(BigNumber.from(priv).toHexString()),
+      ).toBigInt(),
     )
 
-    const merkle_root = F.toObject(poseidon(
-      poseidon([addresses[0], addresses[1]]),
-      poseidon([addresses[2], addresses[3]]),
-    ))
-    const path = [addresses[1], F.toObject(poseidon([addresses[2], addresses[3]]))]
+    const merkleRoot = F.toObject(
+      poseidon(
+        poseidon([addresses[0], addresses[1]]),
+        poseidon([addresses[2], addresses[3]]),
+      ),
+    )
+    const path = [
+      addresses[1],
+      F.toObject(poseidon([addresses[2], addresses[3]])),
+    ]
     const indices = [0, 0]
     const leaf = addresses[0]
 
     const privkey = privkeys[0]
     const pubkey: Point = Point.fromPrivateKey(privkey)
-    const msghash_bigint: bigint = 1234n
-    const msghash: Uint8Array = bigint_to_Uint8Array(msghash_bigint)
-    const sig: Uint8Array = await sign(msghash, bigint_to_Uint8Array(privkey), {canonical: true, der: false})
-    const msghash_array: bigint[] = bigint_to_array(64, 4, msghash_bigint)
+    const msghashBigInt = 1234n
+    const msghash: Uint8Array = bigintToUint8Array(msghashBigInt)
+    const sig: Uint8Array = await sign(msghash, bigintToUint8Array(privkey), {
+      canonical: true,
+      der: false,
+    })
+    const msghashArray: bigint[] = bigintToArray(64, 4, msghashBigInt)
 
-    let witness = await circuit.calculateWitness({
-      'r': bigint_to_array(64, 4, Uint8Array_to_bigint(sig.slice(0, 32))),
-      's': bigint_to_array(64, 4, Uint8Array_to_bigint(sig.slice(32, 64))),
-      'msghash': msghash_array,
-      'pubkey': [
-        bigint_to_array(64, 4, pubkey.x),
-        bigint_to_array(64, 4, pubkey.y)
-      ],
-      'pathElements': path,
-      'pathIndices': indices,
-      'leaf': leaf,
-      'root': merkle_root,
+    const witness = await circuit.calculateWitness({
+      leaf,
+      msghash: msghashArray,
+      pathElements: path,
+      pathIndices: indices,
+      pubkey: [bigintToArray(64, 4, pubkey.x), bigintToArray(64, 4, pubkey.y)],
+      r: bigintToArray(64, 4, uint8ArrayToBigint(sig.slice(0, 32))),
+      root: merkleRoot,
+      s: bigintToArray(64, 4, uint8ArrayToBigint(sig.slice(32, 64))),
     })
 
     await circuit.checkConstraints(witness)
   })
-
-  return
 })
--- circuits/circom/test/helpers.ts
+++ circuits/circom/test/helpers.ts
@@ -1,36 +1,36 @@
 // TODO: import from circom-ecdsa instead
 
 // bigendian
-export function bigint_to_Uint8Array(x: bigint) {
-    var ret: Uint8Array = new Uint8Array(32);
-    for (var idx = 31; idx >= 0; idx--) {
-        ret[idx] = Number(x % 256n);
-        x = x / 256n;
-    }
-    return ret;
+export function bigintToUint8Array(x: bigint) {
+  const ret: Uint8Array = new Uint8Array(32)
+  for (let idx = 31; idx >= 0; idx--) {
+    ret[idx] = Number(x % 256n)
+    x = x / 256n
+  }
+  return ret
 }
 
 // bigendian
-export function Uint8Array_to_bigint(x: Uint8Array) {
-    var ret: bigint = 0n;
-    for (var idx = 0; idx < x.length; idx++) {
-        ret = ret * 256n;
-        ret = ret + BigInt(x[idx]);
-    }
-    return ret;
+export function uint8ArrayToBigint(x: Uint8Array) {
+  let ret = 0n
+  for (let idx = 0; idx < x.length; idx++) {
+    ret = ret * 256n
+    ret = ret + BigInt(x[idx])
+  }
+  return ret
 }
 
-export function bigint_to_array(n: number, k: number, x: bigint) {
-    let mod: bigint = 1n;
-    for (var idx = 0; idx < n; idx++) {
-        mod = mod * 2n;
-    }
+export function bigintToArray(n: number, k: number, x: bigint) {
+  let mod = 1n
+  for (let idx = 0; idx < n; idx++) {
+    mod = mod * 2n
+  }
 
-    let ret: bigint[] = [];
-    var x_temp: bigint = x;
-    for (var idx = 0; idx < k; idx++) {
-        ret.push(x_temp % mod);
-        x_temp = x_temp / mod;
-    }
-    return ret;
+  const ret: bigint[] = []
+  let xTemp: bigint = x
+  for (let idx = 0; idx < k; idx++) {
+    ret.push(xTemp % mod)
+    xTemp = xTemp / mod
+  }
+  return ret
 }
