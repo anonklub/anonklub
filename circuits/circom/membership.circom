f3cff14b4d0de00f02a5c482489d49c7df40fcb7
BlakeMScurr <blakemscurr@gmail.com>
2023-02-03 03:14:45 +1300

    :sprakles:(circom) Prove Non Membership (#76)
    * Non membership circuit.
    
    * Generate exclusion proofs.
    
    * Test exclusion proof generation.
    
    * Test non-membership circuit.
    
    * Linting.
    
    * Reinstate inclusion tests.
    
    * Linting.
    
    * Increase timeout.

--- circuits/circom/membership.circom
+++ circuits/circom/membership.circom
@@ -2,6 +2,8 @@ pragma circom 2.0.2;
 
 include "node_modules/circom-ecdsa/circuits/zk-identity/eth.circom";
 include "node_modules/circom-ecdsa/circuits/ecdsa.circom";
+include "node_modules/circom-ecdsa/node_modules/circomlib/circuits/bitify.circom";
+include "node_modules/circom-ecdsa/node_modules/circomlib/circuits/comparators.circom"; // TODO: fix insane dependency twiddling
 include "./ecdsa.circom";
 include "./merkleTree.circom";
 
@@ -20,6 +22,107 @@ template InAddressSet(n, k, levels) {
     signal input root;
     signal input pathElements[levels];
     signal input pathIndices[levels];
+
+    component validator = ValidateSignatureForAddress(n, k);
+    for (var i = 0; i < k; i++) {
+        validator.pubkey[0][i] <== pubkey[0][i];
+        validator.pubkey[1][i] <== pubkey[1][i];
+        validator.r[i] <== r[i];
+        validator.s[i] <== s[i];
+        validator.msghash[i] <== msghash[i];
+    }
+
+    // Check address is in set
+    component tree = MerkleTreeChecker(levels);
+    tree.leaf <== validator.address;
+    tree.root <== root;
+    for (var i = 0; i < levels; i++) {
+        tree.pathElements[i] <== pathElements[i];
+        tree.pathIndices[i] <== pathIndices[i];
+    }
+}
+
+// Proves that a message is signed by an address which is not in the merkle tree
+// We show that the signer's address would be between two adjacent addresses in a sorted list
+// This assumes that the list is sorted, which should generally be done outside the circuit.
+// If sorted is impossible, you need a different algorithm, as explained here https://crypto.stackexchange.com/a/31915
+template NotInAddressSet(n, k, levels) {
+    signal input pubkey[2][k];
+
+    // Signature
+    signal input r[k];
+    signal input s[k];
+    signal input msghash[k];
+
+    // Merkle proofs
+    signal input root;
+    signal input pathElements[2][levels];
+    signal input pathIndices[2][levels];
+    signal input leaves[2];
+
+    component validator = ValidateSignatureForAddress(n, k);
+    for (var i = 0; i < k; i++) {
+        validator.pubkey[0][i] <== pubkey[0][i];
+        validator.pubkey[1][i] <== pubkey[1][i];
+        validator.r[i] <== r[i];
+        validator.s[i] <== s[i];
+        validator.msghash[i] <== msghash[i];
+    }
+
+    // Make sure the indices are adjacent
+    var belowSum = 0;
+    var aboveSum = 0;
+    for (var i = 0; i < levels; i++) {
+        belowSum += (1 << i) * pathIndices[0][i];
+        aboveSum += (1 << i) * pathIndices[1][i];
+    }
+    aboveSum === belowSum + 1;
+
+    // Check both leaf addresses are in the merkle tree
+    component merkleProofs[2];
+    for (var i = 0; i < 2; i++) {
+        merkleProofs[i] = MerkleTreeChecker(levels);
+        merkleProofs[i].leaf <== leaves[i];
+        merkleProofs[i].root <== root;
+        for (var j = 0; j < levels; j++) {
+            merkleProofs[i].pathElements[j] <== pathElements[i][j];
+            merkleProofs[i].pathIndices[j] <== pathIndices[i][j];
+        }
+    }
+
+    // Make sure below < signer < above
+    // Range check the addresses to make sure they're 160-bit numbers (should in principle be done by the keccak circuit, but it's experimental)
+    component rangeCheck[3];
+    for (var i = 0; i < 3; i++) {
+        rangeCheck[i] = Num2Bits(160);
+    }
+    rangeCheck[0].in <== leaves[0];
+    rangeCheck[1].in <== validator.address;
+    rangeCheck[2].in <== leaves[1];
+
+    component lt[2];
+    for (var i = 0; i < 2; i++) {
+        lt[i] = LessThan(160);
+    }
+
+    lt[0].in[0] <== leaves[0];
+    lt[0].in[1] <== validator.address;
+    lt[0].out === 1;
+
+    lt[1].in[0] <== validator.address;
+    lt[1].in[1] <== leaves[1];
+    lt[1].out === 1;
+}
+
+template ValidateSignatureForAddress(n, k) {
+    signal input pubkey[2][k];
+
+    // Signature
+    signal input r[k];
+    signal input s[k];
+    signal input msghash[k];
+
+    signal output address;
 
     // Validate public key
     component pubKeyCheck = ECDSACheckPubKey(n, k);
@@ -45,17 +148,9 @@ template InAddressSet(n, k, levels) {
         flatten.chunkedPubkey[1][i] <== pubkey[1][i];
     }
 
-    component address = PubkeyToAddress();
+    component pubkeyToAddress = PubkeyToAddress();
     for (var i = 0; i < 512; i++) {
-        address.pubkeyBits[i] <== flatten.pubkeyBits[i];
+        pubkeyToAddress.pubkeyBits[i] <== flatten.pubkeyBits[i];
     }
-
-    // Check address is in set
-    component tree = MerkleTreeChecker(levels);
-    tree.leaf <== address.address;
-    tree.root <== root;
-    for (var i = 0; i < levels; i++) {
-        tree.pathElements[i] <== pathElements[i];
-        tree.pathIndices[i] <== pathIndices[i];
-    }
+    address <== pubkeyToAddress.address;
 }
--- circuits/circom/test/circuit.test.ts
+++ circuits/circom/test/circuit.test.ts
@@ -7,11 +7,14 @@ import { join } from 'path'
 import {
   bigintToArray,
   bigintToUint8Array,
+  ExcludableMerkleTree,
+  maxAddress,
   MerkleTree,
+  minAddress,
   uint8ArrayToBigint,
 } from './helpers'
 
-jest.setTimeout(1_000_000)
+jest.setTimeout(10_000_000) // ~1 hour
 
 describe('Poseidon Merkle Tree', function () {
   let poseidon
@@ -43,6 +46,24 @@ describe('Poseidon Merkle Tree', function () {
     const tree = new MerkleTree([0n, 1n, 2n, 3n], 3, poseidon, F)
 
     expect(tree.merkleProof(1)).toEqual({ pathElements, pathIndices })
+  })
+
+  it('Should generate exclusion proofs', () => {
+    expect(
+      new ExcludableMerkleTree([10n, 1000n], 3, poseidon, F).exclusionProof(
+        99n,
+      ),
+    ).toEqual({
+      leaves: [10n, 1000n],
+      pathElements: [
+        [minAddress, F.toObject(poseidon([1000n, maxAddress]))],
+        [maxAddress, F.toObject(poseidon([minAddress, 10n]))],
+      ],
+      pathIndices: [
+        [1, 0],
+        [0, 1],
+      ],
+    })
   })
 
   it('Should generate merkle roots', () => {
@@ -141,28 +162,34 @@ describe('Poseidon Merkle Tree', function () {
 describe('SetMembership', function () {
   let poseidon
   let F
-  let circuit
+  let membershipCircuit
+  let nonMembershipCircuit
+
+  const privkeys: bigint[] = [
+    88549154299169935420064281163296845505587953610183896504176354567359434168161n,
+    37706893564732085918706190942542566344879680306879183356840008504374628845468n,
+    90388020393783788847120091912026443124559466591761394939671630294477859800601n,
+    110977009687373213104962226057480551605828725303063265716157300460694423838923n,
+  ]
+
+  const addresses = privkeys.map((priv) =>
+    BigNumber.from(
+      utils.computeAddress(BigNumber.from(priv).toHexString()),
+    ).toBigInt(),
+  )
 
   beforeAll(async function () {
-    circuit = await wasm_tester(join(__dirname, 'membership_test.circom'))
+    membershipCircuit = await wasm_tester(
+      join(__dirname, 'membership_test.circom'),
+    )
+    nonMembershipCircuit = await wasm_tester(
+      join(__dirname, 'non_membership_test.circom'),
+    )
     poseidon = await buildPoseidon()
     F = poseidon.F // TODO: do we actually need this or is it the default field?
   })
 
-  it('Should produce valid proofs', async () => {
-    const privkeys: bigint[] = [
-      88549154299169935420064281163296845505587953610183896504176354567359434168161n,
-      37706893564732085918706190942542566344879680306879183356840008504374628845468n,
-      90388020393783788847120091912026443124559466591761394939671630294477859800601n,
-      110977009687373213104962226057480551605828725303063265716157300460694423838923n,
-    ]
-
-    const addresses = privkeys.map((priv) =>
-      BigNumber.from(
-        utils.computeAddress(BigNumber.from(priv).toHexString()),
-      ).toBigInt(),
-    )
-
+  it('Should accept valid inclusion proofs', async () => {
     const tree = new MerkleTree(addresses, 3, poseidon, F)
     const merkleProof = tree.merkleProof(0)
 
@@ -176,7 +203,7 @@ describe('SetMembership', function () {
     })
     const msghashArray: bigint[] = bigintToArray(64, 4, msghashBigint)
 
-    const witness = await circuit.calculateWitness({
+    const witness = await membershipCircuit.calculateWitness({
       msghash: msghashArray,
       pathElements: merkleProof.pathElements,
       pathIndices: merkleProof.pathIndices,
@@ -186,7 +213,39 @@ describe('SetMembership', function () {
       s: bigintToArray(64, 4, uint8ArrayToBigint(sig.slice(32, 64))),
     })
 
-    await circuit.checkConstraints(witness)
+    await membershipCircuit.checkConstraints(witness)
+  })
+
+  it('Should accept valid exclusion proofs', async () => {
+    // Generate exclusion proof
+    const inTree = [addresses[1], addresses[2]]
+    const tree = new ExcludableMerkleTree(inTree, 3, poseidon, F)
+    const exclusionProof = tree.exclusionProof(addresses[0])
+
+    // Sign a message
+    const privkey = privkeys[0]
+    const pubkey: Point = Point.fromPrivateKey(privkey)
+    const msghashBigint = 1234n
+    const msghash: Uint8Array = bigintToUint8Array(msghashBigint)
+    const sig: Uint8Array = await sign(msghash, bigintToUint8Array(privkey), {
+      canonical: true,
+      der: false,
+    })
+    const msghashArray: bigint[] = bigintToArray(64, 4, msghashBigint)
+
+    // Make ZK proof witness
+    const witness = await nonMembershipCircuit.calculateWitness({
+      leaves: exclusionProof.leaves,
+      msghash: msghashArray,
+      pathElements: exclusionProof.pathElements,
+      pathIndices: exclusionProof.pathIndices,
+      pubkey: [bigintToArray(64, 4, pubkey.x), bigintToArray(64, 4, pubkey.y)],
+      r: bigintToArray(64, 4, uint8ArrayToBigint(sig.slice(0, 32))),
+      root: tree.root(),
+      s: bigintToArray(64, 4, uint8ArrayToBigint(sig.slice(32, 64))),
+    })
+
+    await nonMembershipCircuit.checkConstraints(witness)
   })
 })
 
