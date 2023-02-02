67d1fc3665d384ceb6069f0b79c50c6a9894a651
r1oga <r1oga@tuta.io>
2023-02-02 11:32:31 +0000

    :sparkles:(query-api) Serve swagger ui on root endpoint (#81)
    * chore: Add swagger ui express dep
    
    * :sparkles: Serve swagger UI on root endpoint (#80)
    
    * :memo: Update README
    
    * add open api spec
    
    * enable cors
    
    * :sparkles: Serve swagger UI on root endpoint
    
    * format
    
    * update readme
    
    * update workflows trigger
    
    * fix test
    
    * fix: copy openapi specs into docker image
    
    * update open api specs
    
    * customize favicon and root page title
    
    * format

--- apis/query/src/app.ts
+++ apis/query/src/app.ts
@@ -1,10 +1,33 @@
+import express from 'express'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { createExpressServer, useContainer } from 'routing-controllers-extended'
+import swaggerUi from 'swagger-ui-express'
 import { Container } from 'typedi'
-
+import { parse } from 'yaml'
 import { controllers, middlewares } from './config'
 
+const openApiSpecYaml = readFileSync(
+  join(__dirname, '..', 'openapi.yaml'),
+  'utf8',
+)
+
 useContainer(Container)
-export const app = createExpressServer({
+
+const app = createExpressServer({
   controllers,
+  cors: true,
   middlewares,
 })
+
+app.use(express.static('public'))
+app.use('/', swaggerUi.serve)
+app.get(
+  '/',
+  swaggerUi.setup(parse(openApiSpecYaml), {
+    customfavIcon: '/favicon.ico',
+    customSiteTitle: 'Anon Set API',
+  }),
+)
+
+export { app }
