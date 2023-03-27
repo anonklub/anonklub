6bda224b83beba901ae43e97ab52730bb8e80346
r1oga <me@r1oga.io>
2023-03-27 23:15:03 +0200

    :bug: Reset Dune client on query errors (#112)
    * fix: reset Dune client on query error
    
    * add TODO

--- apis/query/src/api/repositories/DuneRepository.ts
+++ apis/query/src/api/repositories/DuneRepository.ts
@@ -16,7 +16,14 @@ export class DuneRepository {
     })
   }
 
-  public async queryErc20Balance({
+  private resetDune() {
+    this.dune = new Dune({
+      password: process.env.DUNE_PASSWORD,
+      username: process.env.DUNE_USERNAME,
+    })
+  }
+
+  private async _queryErc20Balance({
     min,
     tokenAddress,
   }: {
@@ -27,5 +34,22 @@ export class DuneRepository {
       { key: 'tokenAddress', type: ParameterType.Text, value: tokenAddress },
       { key: 'min', type: ParameterType.Number, value: min },
     ])
+  }
+
+  public async queryErc20Balance({
+    min,
+    tokenAddress,
+  }: {
+    min: string
+    tokenAddress: string
+  }) {
+    try {
+      return this._queryErc20Balance({ min, tokenAddress })
+    } catch (err) {
+      console.log('DuneRepository.queryErc20Balance() error:', err)
+      // TODO: not sure if this is reliable fix, look into dune-ts instead (cache of cookies/bearer token?)
+      this.resetDune()
+      return this._queryErc20Balance({ min, tokenAddress })
+    }
   }
 }
