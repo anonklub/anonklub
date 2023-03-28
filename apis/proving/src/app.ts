378778fd20a48b159fc013021e9f7886ddca169a
r1oga <38692952+r1oga@users.noreply.github.com>
2023-03-28 17:02:48 +0200

    fix: increase express payload size limit

--- apis/proving/src/app.ts
+++ apis/proving/src/app.ts
@@ -3,6 +3,6 @@ import express from 'express'
 import { provingRouter } from './routes'
 
 const app = express()
-app.use(express.json(), provingRouter)
+app.use(express.json({ limit: '1mb' }), provingRouter)
 
 export { app }
