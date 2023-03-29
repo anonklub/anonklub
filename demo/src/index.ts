0da65a09eb5d5a90ba90b0e89b334ed854a4451d
r1oga <38692952+r1oga@users.noreply.github.com>
2023-03-29 18:01:49 +0200

    update script

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,4 +1,5 @@
 import delay from 'delay'
+import { existsSync } from 'fs'
 import { readFile, writeFile } from 'fs/promises'
 import { hideBin } from 'yargs/helpers'
 import yargs from 'yargs/yargs'
@@ -7,6 +8,7 @@ import { fetchProof } from './fetch-proof'
 
 interface Args {
   erc20Address: string
+  fetchAnonSet: boolean
   minBalance: string
   privateKey: string
 }
@@ -14,23 +16,27 @@ interface Args {
 async function main() {
   const argv = yargs(hideBin(process.argv)).options({
     erc20Address: { alias: 'a', requiresArg: true, type: 'string' },
+    fetchAnonSet: { alias: 'f', requiresArg: false, type: 'boolean' },
     minBalance: { alias: 'm', requiresArg: true, type: 'string' },
     privateKey: { alias: 'p', requiresArg: true, type: 'string' },
   }).argv as Args
 
-  console.log('Fetching addresses from query API')
+  let addresses: string[]
+
+  if (argv.fetchAnonSet || !existsSync('addresses.json')) {
+    console.log('Fetching addresses from query API')
+
+    addresses = await fetchErc20AnonSet({
+      min: argv.minBalance,
+      tokenAddress: argv.erc20Address,
+    })
+    await writeFile('addresses.json', JSON.stringify(addresses))
+  } else {
+    console.log('Loading addresses from file')
+    addresses = JSON.parse(await readFile('addresses.json', 'utf-8'))
+  }
+
   await delay(300)
-  // const addresses = await fetchErc20AnonSet({
-  //   min: argv.minBalance,
-  //   tokenAddress: argv.erc20Address,
-  // })
-  //
-  //
-  // await writeFile('addresses.json', JSON.stringify(addresses))
-
-  // loading addresses from file
-  const addresses = JSON.parse(await readFile('addresses.json', 'utf-8'))
-
   console.log(addresses.slice(0, 10))
 
   await fetchProof({
