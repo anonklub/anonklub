2062eb994e07a164df768f78e0b6b162ddd3be3e
r1oga <38692952+r1oga@users.noreply.github.com>
2023-03-30 18:44:48 +0200

    test ProofRequest class

--- shared/src/ProofRequest.ts
+++ shared/src/ProofRequest.ts
@@ -1,46 +1,27 @@
 import { Point } from '@noble/secp256k1'
-import { stringifyWithBigInts } from './helpers'
-
-export interface ProofRequestArgs {
-  addresses: bigint[]
-  signature: Uint8Array
-  msghash: bigint
-  addressIndex: number
-  pubkey: Point
-}
+import { utils } from 'ethers'
 
 export class ProofRequest {
-  addresses: bigint[]
-  signature: Uint8Array
-  msghash: bigint
-  addressIndex: number
-  pubkey: Point
+  readonly addresses: string[]
+  readonly message: string
+  readonly publicKey: { x: string; y: string }
+  readonly rawSignature: string
 
   constructor({
     addresses,
-    addressIndex,
-    msghash,
-    pubkey,
-    signature,
-  }: ProofRequestArgs) {
+    message,
+    rawSignature,
+  }: {
+    addresses: string[]
+    rawSignature: string
+    message: string
+  }) {
     this.addresses = addresses
-    this.signature = signature
-    this.msghash = msghash
-    this.addressIndex = addressIndex
-    this.pubkey = pubkey
-  }
-
-  stringify() {
-    return stringifyWithBigInts(this)
-  }
-
-  static fromReq(req) {
-    for (let i = 0; i < req.addresses.length; i++) {
-      req.addresses[i] = BigInt(req.addresses[i])
-    }
-    req.pubkey = new Point(BigInt(req.pubkey.x), BigInt(req.pubkey.y))
-    req.msghash = BigInt(req.msghash)
-    req.signature = Uint8Array.from(Object.values(req.signature))
-    return req
+    this.rawSignature = rawSignature
+    const publicKey = Point.fromHex(
+      utils.recoverPublicKey(utils.hashMessage(message), rawSignature).slice(2),
+    )
+    this.publicKey = { x: publicKey.x.toString(), y: publicKey.y.toString() }
+    this.message = message
   }
 }
--- shared/test/ProofRequest.test.ts
+++ shared/test/ProofRequest.test.ts
@@ -1,70 +1,27 @@
-import { hmac } from '@noble/hashes/hmac'
-import { sha256 } from '@noble/hashes/sha256'
-import { Point, signSync, utils as secp256k1utils } from '@noble/secp256k1'
-import { BigNumber, utils } from 'ethers'
+import { utils, Wallet } from 'ethers'
+import { ProofRequest } from '../src'
 
-import {
-  bigintToUint8Array,
-  CircuitInput,
-  memoPoseidon,
-  ProofRequest,
-  stringifyWithBigInts,
-} from '../src'
-
-import proofRequestJson from './data/input.json'
-
-secp256k1utils.hmacSha256Sync = (key, ...msgs) =>
-  hmac(sha256, key, secp256k1utils.concatBytes(...msgs))
-secp256k1utils.sha256Sync = (...msgs) =>
-  sha256(secp256k1utils.concatBytes(...msgs))
-
-const privkeys: bigint[] = [
-  88549154299169935420064281163296845505587953610183896504176354567359434168161n,
-  37706893564732085918706190942542566344879680306879183356840008504374628845468n,
-  90388020393783788847120091912026443124559466591761394939671630294477859800601n,
-  110977009687373213104962226057480551605828725303063265716157300460694423838923n,
-]
-
-const addresses = privkeys.map((priv) =>
-  BigNumber.from(
-    utils.computeAddress(BigNumber.from(priv).toHexString()),
-  ).toBigInt(),
-)
+const message = 'Hello World'
+const wallets = [...Array(10)].map(() => Wallet.createRandom())
+const addresses = wallets.map((wallet) => wallet.address)
+const wallet = wallets[Math.floor(Math.random() * wallets.length)]
 
 describe('ProofRequest', () => {
-  let poseidon: any
-  beforeAll(async () => {
-    poseidon = await memoPoseidon()
-  })
-  it('should stringify', () => {
-    const addressIndex = 0
-    const privkey = privkeys[addressIndex]
-    const pubkey: Point = Point.fromPrivateKey(privkey)
-    const msghash = 1234n
-    const msghashArray: Uint8Array = bigintToUint8Array(msghash)
-    const signature: Uint8Array = signSync(
-      msghashArray,
-      bigintToUint8Array(privkey),
-      {
-        canonical: true,
-        der: false,
-      },
-    )
-    const postData = new ProofRequest({
+  it('creates Proof Request Instance with right properties', async () => {
+    const rawSignature = await wallet.signMessage(message)
+    const proofRequest = new ProofRequest({
       addresses,
-      addressIndex,
-      msghash,
-      pubkey,
-      signature,
-    }).stringify()
-
-    const circuitInput = new CircuitInput({
-      poseidon,
-      proofRequest: JSON.parse(postData),
+      message,
+      rawSignature,
     })
 
-    expect(JSON.parse(stringifyWithBigInts(circuitInput))).toEqual(
-      proofRequestJson,
-    )
+    expect(proofRequest.addresses).toEqual(addresses)
+    proofRequest.addresses.forEach((address) => {
+      utils.isAddress(address)
+    })
+    expect(proofRequest.message).toEqual(message)
+    expect(proofRequest.rawSignature).toEqual(rawSignature)
+    expect(proofRequest.publicKey.x).toMatch(/^[0-9]+$/)
+    expect(proofRequest.publicKey.y).toMatch(/^[0-9]+$/)
   })
 })
