0cb3787674aafa96734ca64348227d345932978b
r1oga <38692952+r1oga@users.noreply.github.com>
2023-04-05 08:45:37 +0200

    format/lint

--- apis/proving/src/dashboard.ts
+++ apis/proving/src/dashboard.ts
@@ -1,5 +1,5 @@
-import { BullMQAdapter } from '@bull-board/api/bullMQAdapter'
 import { createBullBoard } from '@bull-board/api'
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter'
 import { ExpressAdapter } from '@bull-board/express'
 import { queue } from './mq/queue'
 
