b959561aa18d4b2311444fd8aa25f5c3db198847
r1oga <38692952+r1oga@users.noreply.github.com>
2023-04-05 09:06:21 +0200

    generate random ID for each job

--- apis/proving/src/handlers/proof.ts
+++ apis/proving/src/handlers/proof.ts
@@ -1,7 +1,18 @@
+import { createHash } from 'crypto'
 import { queue } from '../mq/queue'
+
+const sha256 = createHash('sha256')
+sha256.update('some salt') // TODO: use a real secret salt
+
+const generateId = (proofRequestBody: any) => {
+  sha256.update(JSON.stringify(proofRequestBody))
+  return sha256.digest('hex')
+}
 
 export const proofHandler = async (req, res) => {
   const proofRequest = req.body
-  const job = await queue.add('prove', proofRequest)
+  const job = await queue.add('prove', proofRequest, {
+    jobId: generateId(proofRequest),
+  })
   res.json(job)
 }
