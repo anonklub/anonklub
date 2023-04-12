23f6e1d824078b892e0385a5bd77ead9e06fb24e
3pwd <me@3pwd.xyz>
2023-04-12 17:55:46 +0200

    use inquirer instead to collect user inputs

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,8 +1,8 @@
 import delay from 'delay'
+import { ethers } from 'ethers'
 import { existsSync } from 'fs'
 import { readFile, writeFile } from 'fs/promises'
-import { hideBin } from 'yargs/helpers'
-import yargs from 'yargs/yargs'
+import inquirer from 'inquirer'
 import { fetchErc20AnonSet } from './fetch-anon-set'
 import { fetchProof } from './fetch-proof'
 
@@ -13,22 +13,50 @@ interface Args {
   privateKey: string
 }
 
+const QUESTIONS = [
+  {
+    default: '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72',
+    message: 'ERC20 address you want to query',
+    name: 'erc20Address',
+    suffix: ' [Example: 0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72]',
+    type: 'string',
+    validate: (answer: string) =>
+      ethers.utils.isAddress(answer) || 'Invalid address',
+  },
+  {
+    message:
+      'Minimum balance of ERC20 one must own to be part of the anonymity set',
+    name: 'minBalance',
+    suffix: ' [Example: 10000]',
+    type: 'string',
+    validate: (answer: string) =>
+      answer.match(/^[0-9]+$/)?.length !== undefined || 'Invalid number',
+  },
+  {
+    message:
+      'Private key of the address you want to prove is part of the anonymity set',
+    name: 'privateKey',
+    suffix:
+      ' [Example: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef]',
+    type: 'string',
+    validate: (answer: string) =>
+      answer.match(/^0x[0-9a-fA-F]{64}$/)?.length !== undefined ||
+      'Invalid private key',
+  },
+]
+
 async function main() {
-  const argv = yargs(hideBin(process.argv)).options({
-    erc20Address: { alias: 'a', requiresArg: true, type: 'string' },
-    fetchAnonSet: { alias: 'f', requiresArg: false, type: 'boolean' },
-    minBalance: { alias: 'm', requiresArg: true, type: 'string' },
-    privateKey: { alias: 'p', requiresArg: true, type: 'string' },
-  }).argv as Args
+  const { erc20Address, fetchAnonSet, minBalance, privateKey }: Args =
+    await inquirer.prompt(QUESTIONS)
 
   let addresses: string[]
 
-  if (argv.fetchAnonSet || !existsSync('addresses.json')) {
+  if (fetchAnonSet || !existsSync('addresses.json')) {
     console.log('Fetching addresses from query API')
 
     addresses = await fetchErc20AnonSet({
-      min: argv.minBalance,
-      tokenAddress: argv.erc20Address,
+      min: minBalance,
+      tokenAddress: erc20Address,
     })
     await writeFile('addresses.json', JSON.stringify(addresses))
   } else {
@@ -39,9 +67,10 @@ async function main() {
   await delay(300)
   console.log(addresses.slice(0, 10))
 
+  // TODO: refactor so that a pre-generated signature is passed in instead of private key
   await fetchProof({
     addresses,
-    privateKey: argv?.privateKey,
+    privateKey,
   })
 }
 
