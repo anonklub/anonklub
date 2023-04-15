2099b82445dc1ccdbded78583c84e1d93ef4e704
3pwd <me@3pwd.xyz>
2023-04-15 22:10:26 +0200

    :recycle: Extract cli result logs into separate file

--- demo/src/cli/index.ts
+++ demo/src/cli/index.ts
@@ -1,7 +1,7 @@
-import terminalLink from 'terminal-link'
 import { ProofRequest } from '@anonset/membership'
 import { API_URLS } from '../constants'
 import { fetchErc20AnonSet } from '../fetch-anon-set'
+import { logResult } from './log-result'
 import {
   AnonSetLocation,
   AnonSetType,
@@ -68,31 +68,8 @@ export const cli = async () => {
       })
 
       const { jobId } = await proofRequest.submit()
-      console.log(
-        'Proof Request submitted successfully and being now processed.',
-      )
-      console.log(
-        `Your job id is ${jobId}. Do not share this id. You'll need it to access your proof file.`,
-      )
-      console.log(
-        `Your proof input is already available at ${terminalLink(
-          'input.json',
-          `${API_URLS.PROVE}/proofs/${jobId}/input.json`,
-        )}`,
-      )
-      console.log('Wait 5-10 min and your results will be available at:')
-      console.log(
-        terminalLink(
-          'proof.json',
-          `${API_URLS.PROVE}/proofs/${jobId}/proof.json`,
-        ),
-      )
-      console.log(
-        terminalLink(
-          'publicSignals.json',
-          `${API_URLS.PROVE}/proofs/${jobId}/publicSignals.json`,
-        ),
-      )
+      logResult(jobId)
+
       break
     }
     case ProofAction.VERIFY:
