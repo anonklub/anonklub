74146f637987a39bcf03ca53435361944d6e685c
3pwd <me@3pwd.xyz>
2023-04-15 21:52:42 +0200

    render url as hyperlinks in terminal

--- demo/src/cli/index.ts
+++ demo/src/cli/index.ts
@@ -1,3 +1,4 @@
+import terminalLink from 'terminal-link'
 import { ProofRequest } from '@anonset/membership'
 import { API_URLS } from '../constants'
 import { fetchErc20AnonSet } from '../fetch-anon-set'
@@ -15,7 +16,7 @@ import {
   ProofAction,
 } from './prompts'
 
-export async function cli() {
+export const cli = async () => {
   const proveOrVerify = await askProveOrVerify()
 
   switch (proveOrVerify) {
@@ -66,8 +67,32 @@ export async function cli() {
         url: API_URLS.PROVE,
       })
 
-      const jobResponse = await proofRequest.submit()
-      console.log(jobResponse)
+      const { jobId } = await proofRequest.submit()
+      console.log(
+        'Proof Request submitted successfully and being now processed.',
+      )
+      console.log(
+        `Your job id is ${jobId}. Do not share this id. You'll need it to access your proof file.`,
+      )
+      console.log(
+        `Your proof input is already available at ${terminalLink(
+          'input.json',
+          `${API_URLS.PROVE}/proofs/${jobId}/input.json`,
+        )}`,
+      )
+      console.log('Wait 5-10 min and your results will be available at:')
+      console.log(
+        terminalLink(
+          'proof.json',
+          `${API_URLS.PROVE}/proofs/${jobId}/proof.json`,
+        ),
+      )
+      console.log(
+        terminalLink(
+          'publicSignals.json',
+          `${API_URLS.PROVE}/proofs/${jobId}/publicSignals.json`,
+        ),
+      )
       break
     }
     case ProofAction.VERIFY:
