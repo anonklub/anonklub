9a26a7822e2ef4bb5e96279d5a1bdc79ded2363c
3pwd <me@3pwd.xyz>
2023-04-16 21:04:20 +0200

    :sparkles: Can fetch other type of anon sets from cli

--- demo/src/cli/prompts/ask-eth-anonset-inputs.ts
+++ demo/src/cli/prompts/ask-eth-anonset-inputs.ts
@@ -1,2 +1,10 @@
-export const askEthAnonsetInputs = async () =>
-  new Promise((resolve) => resolve('todo'))
+import { prompt } from './prompt'
+
+export const askEthAnonsetInputs = prompt<{ minBalance: string }>({
+  message:
+    'What is the integer minimum balance (in ETH) addresses should hold?',
+  name: 'minBalance',
+  type: 'input',
+  validate: (answer: string) =>
+    answer.match(/^[0-9]+$/) !== null || 'Invalid integer',
+})
