0441df288c4ff9a6256af50ff9341f4e6ab612ae
Threepwood <me@3pwd.xyz>
2023-04-26 16:32:24 +0200

    :bug: Re add deleted helpers used in circom tests (#141)
    * :bug: Re add deleted helpers used in circom tests
    
    * remove test_client
    
    * fix helpers exports
    
    * update package.json, tsconfig.json

--- membership/src/helpers.ts
+++ membership/src/helpers.ts
@@ -13,6 +13,25 @@ export function hashLevel(level: bigint[], h, f): bigint[] {
   return nextLevel
 }
 
+export function bigintToUint8Array(x: bigint) {
+  const ret: Uint8Array = new Uint8Array(32)
+  for (let idx = 31; idx >= 0; idx--) {
+    ret[idx] = Number(x % 256n)
+    x = x / 256n
+  }
+  return ret
+}
+
+// bigendian
+export function uint8ArrayToBigint(x: Uint8Array) {
+  let ret = 0n
+  for (let idx = 0; idx < x.length; idx++) {
+    ret = ret * 256n
+    ret = ret + BigInt(x[idx])
+  }
+  return ret
+}
+
 export function bigintToArray(n: number, k: number, x: bigint) {
   let mod = 1n
   for (let idx = 0; idx < n; idx++) {
--- membership/src/index.ts
+++ membership/src/index.ts
@@ -1,4 +1,13 @@
 export * from './CircuitInput'
+export { ExcludableMerkleTree } from './ExcludableMerkleTree'
+export {
+  bigintToArray,
+  bigintToUint8Array,
+  maxAddress,
+  minAddress,
+  uint8ArrayToBigint,
+} from './helpers'
+export { MerkleTree } from './MerkleTree'
 export * from './poseidon'
 export * from './ProofRequest'
 export { ProofRequestJson } from './ProofRequest/interface'
