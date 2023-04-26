ad03aa8e59125b5db0fe76afb78bd0736c81c8b9
3pwd <me@3pwd.xyz>
2023-04-26 20:56:05 +0200

    wip update pulumi config files, remove namespace?

--- infra/resources/cluster/namespace.ts
+++ infra/resources/cluster/namespace.ts
@@ -1,6 +1,11 @@
 import { core } from '@pulumi/kubernetes'
 import { provider } from './provider'
 
+// FIXME
+// necessary?, using a namespace causes issues when trying to delete it
+// Timeout occurred polling
+// https://github.com/kubernetes/kubernetes/issues/60807
+
 const ns = new core.v1.Namespace(
   'anonklub',
   {},
