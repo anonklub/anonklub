c5a372cc63f2b6d480a5db3e8113c9eb55119433
3pwd <me@3pwd.xyz>
2023-04-26 14:39:02 +0200

    :bug: Re add deleted helpers used in circom tests

--- membership/src/index.ts
+++ membership/src/index.ts
@@ -1,4 +1,11 @@
 export * from './CircuitInput'
+export {
+  bigintToArray,
+  bigintToUint8Array,
+  maxAddress,
+  minAddress,
+  uint8ArrayToBigint,
+} from './helpers'
 export * from './poseidon'
 export * from './ProofRequest'
 export { ProofRequestJson } from './ProofRequest/interface'
