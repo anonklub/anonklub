ffce5bdcf79ba25b8855a59ebcf569f1f2bf55ca
3pwd <me@3pwd.xyz>
2023-05-08 14:29:43 +0200

    test dune ts query

--- apis/query/test/unit/DuneRepository.test.ts
+++ apis/query/test/unit/DuneRepository.test.ts
@@ -1,9 +1,45 @@
+import { faker } from '@faker-js/faker'
 import { DuneRepository } from '@repositories'
 
+jest.setTimeout(30_000)
+
 describe('DuneRepository', () => {
+  const ENV = process.env
+
+  beforeEach(() => {
+    process.env = { ...ENV }
+  })
+
+  afterAll(() => {
+    process.env = ENV
+  })
+
   it('needs DUNE_API_KEY', () => {
     delete process.env.DUNE_API_KEY
 
     expect(() => new DuneRepository()).toThrow('missing dune api key')
+  })
+
+  it('fetch query results', async () => {
+    const duneRepository = new DuneRepository()
+    const min = '3000'
+    const tokenAddress = '0xc18360217d8f7ab5e7c516566761ea12ce7f9d72'
+
+    jest.spyOn(duneRepository.dune, 'refresh').mockResolvedValueOnce({
+      // @ts-expect-error don't bother with the full response
+      result: {
+        rows: [...Array(faker.datatype.number({ max: 10 })).keys()].map(() => ({
+          address: faker.finance.ethereumAddress(),
+        })),
+      },
+    })
+    const { result } = await duneRepository.queryErc20Balance({
+      min,
+      tokenAddress,
+    })
+
+    expect(result?.rows).toBeDefined()
+    expect(result?.rows.length).toBeGreaterThan(0)
+    expect(result?.rows[0].address.startsWith('0x')).toBe(true)
   })
 })
