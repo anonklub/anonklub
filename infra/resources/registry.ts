d90e9577c966672001e5798fe3c7b5ac18e65ca9
3pwd <me@3pwd.xyz>
2023-05-10 16:39:03 +0200

    use EU registry

--- infra/resources/registry.ts
+++ infra/resources/registry.ts
@@ -1,6 +1,9 @@
 import { container } from '@pulumi/gcp'
 
-export const registry = new container.Registry('registry')
+const registry = new container.Registry('registry', { location: 'EU' })
 export const registryUrl = registry.id.apply(async (_) =>
   container.getRegistryRepository().then((reg) => reg.repositoryUrl),
 )
+
+// FIXME: had to set registry visibility to public manually in GCP web console
+// how to be able to pull from private registry with pulumi?
