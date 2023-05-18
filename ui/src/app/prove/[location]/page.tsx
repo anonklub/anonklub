09858a9131483d2ceedc6ae1633814d6b13bff0c
3pwd <me@3pwd.xyz>
2023-05-18 17:57:24 +0200

    Name Page React Components

--- ui/src/app/prove/[location]/page.tsx
+++ ui/src/app/prove/[location]/page.tsx
@@ -1,19 +1,21 @@
 import Screen from '@components/Screen'
 
-export default () => (
-  <Screen
-    question='What type of membership do you want to prove?'
-    help={[
-      'Cryptopunk: are you a member of the group of people who own a cryptopunk?',
-      'ENS Proposal Voters: are you a member of the group of people who voted on a specific ENS proposal.',
-      'ETH balance: are you a member of the group of people who own a min amount of ETH?',
-      'ERC20 balance: are you a member of the group of people who own a min amount of a given ERC20 token?',
-    ]}
-    buttons={[
-      { href: '/query/cryptopunk', text: 'Cryptopunk' },
-      { href: '/query/ens', text: 'ENS Proposal Voters' },
-      { href: '/query/eth-balance', text: 'ETH balance' },
-      { href: '/query/erc20-balance', text: 'ERC20 balance' },
-    ]}
-  />
-)
+export default function ChooseAnonSetTypePage() {
+  return (
+    <Screen
+      question='What type of membership do you want to prove?'
+      help={[
+        'Cryptopunk: are you a member of the group of people who own a cryptopunk?',
+        'ENS Proposal Voters: are you a member of the group of people who voted on a specific ENS proposal.',
+        'ETH balance: are you a member of the group of people who own a min amount of ETH?',
+        'ERC20 balance: are you a member of the group of people who own a min amount of a given ERC20 token?',
+      ]}
+      buttons={[
+        { href: '/query/cryptopunk', text: 'Cryptopunk' },
+        { href: '/query/ens', text: 'ENS Proposal Voters' },
+        { href: '/query/eth-balance', text: 'ETH balance' },
+        { href: '/query/erc20-balance', text: 'ERC20 balance' },
+      ]}
+    />
+  )
+}
--- ui/src/app/prove/page.tsx
+++ ui/src/app/prove/page.tsx
@@ -1,16 +1,18 @@
 import Screen from '@components/Screen'
 
-export default () => (
-  <Screen
-    question='Where does your anon set lives?'
-    help={[
-      "Anon set: a list of addresses you want to anonymously you're part of.",
-      "On chain: we'll fetch the list of addresses for you from the blockchain (using providers such as Google BigQuery, Dune Analytics or The Graph depending on the anon set type).",
-      "Locally: you'll need to upload a json file containing a list of addresses you collected yourself beforehand.",
-    ]}
-    buttons={[
-      { href: '/prove/on-chain', text: 'On chain' },
-      { href: '/prove/file', text: 'Locally (file)' },
-    ]}
-  />
-)
+export default function ProvePage() {
+  return (
+    <Screen
+      question='Where does your anon set lives?'
+      help={[
+        "Anon set: a list of addresses you want to anonymously you're part of.",
+        "On chain: we'll fetch the list of addresses for you from the blockchain (using providers such as Google BigQuery, Dune Analytics or The Graph depending on the anon set type).",
+        "Locally: you'll need to upload a json file containing a list of addresses you collected yourself beforehand.",
+      ]}
+      buttons={[
+        { href: '/prove/on-chain', text: 'On chain' },
+        { href: '/prove/file', text: 'Locally (file)' },
+      ]}
+    />
+  )
+}
