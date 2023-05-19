d09b24a0914ccd9a49bdefdf14bde1e8951cb250
3pwd <me@3pwd.xyz>
2023-05-19 11:29:11 +0200

    update config interface

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -1,18 +1,23 @@
 import { sepolia } from 'wagmi/chains'
 
-function validateConfig(config: any) {
+interface Config {
+  appName: string
+  chains: any[]
+  urls: {
+    alchemy: string
+    queryApi: string
+  }
+  walletConnectProjectId: string
+}
+
+function validateConfig(config: any): Config {
   Object.keys(config).forEach((key) => {
     const value = config[key]
     if (value === undefined || value === null)
       console.error(`${key} is not set`)
   })
 
-  return config as {
-    alchemyRpcUrl: string
-    appName: string
-    chains: any[]
-    walletConnectProjectId: string
-  }
+  return config
 }
 
 const APP_NAME = 'My App'
@@ -24,9 +29,12 @@ const alchemyRpcUrl = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
 const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
 
 const config = {
-  alchemyRpcUrl,
   appName: APP_NAME,
   chains: [sepolia],
+  urls: {
+    alchemy: alchemyRpcUrl,
+    queryApi: 'https://anonset-staging.fly.dev/',
+  },
   walletConnectProjectId,
 }
 
