4e22450ae1bfcd2fb5aaa33a2cb875cd0b26062e
3pwd <me@3pwd.xyz>
2023-05-19 15:00:13 +0200

    refactor Cryptopunk query page

--- ui/src/app/query/cryptopunk/page.tsx
+++ ui/src/app/query/cryptopunk/page.tsx
@@ -1,21 +1,34 @@
-import config from '#/config'
-import { getData } from '#/get-data'
+// import config from '#/config'
+// import { getData } from '#/get-data'
+import Link from 'next/link'
+import { RESULTS as anonSet } from '@/app/query/cryptopunk/_results'
 
 export default async function Page() {
-  const results = await getData<string[]>(`${config.urls.queryApi}/punks`)
+  // const results = await getData<string[]>(`${config.urls.queryApi}/punks`)
+  await new Promise((resolve, reject) => setTimeout(resolve, 1000))
 
   return (
     <>
-      <h2>Cryptopunk Anon Set</h2>
-      <div className='flex flex-col space-y-3'>
-        <h3>Results</h3>
-        <div className='nes-container is-rounded flex max-h-96 flex-row flex-wrap justify-evenly overflow-auto scroll-smooth'>
-          {results.map((result, key) => (
-            <span key={key} className='text-xs'>
-              {result}
-            </span>
-          ))}
+      <div className='mb-3 flex flex-row justify-between'>
+        <div>
+          <h2>Cryptopunk Anon Set</h2>
+          <h3 className='nes-text is-success'>Results</h3>
         </div>
+        <Link
+          href={{
+            pathname: '/prove/from-on-chain/cryptopunk',
+            query: { anonSet },
+          }}
+        >
+          <button className='nes-btn is-success'>Next: Prove</button>
+        </Link>
+      </div>
+      <div className='nes-container is-rounded flex max-h-96 flex-row flex-wrap justify-evenly overflow-auto scroll-smooth'>
+        {anonSet.map((result, key) => (
+          <span key={key} className='text-xs'>
+            {result}
+          </span>
+        ))}
       </div>
     </>
   )
