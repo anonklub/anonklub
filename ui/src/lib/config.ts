c626b3bf7b7f1b7143e9b0143525b402a7bcb9f6
Threepwood <me@3pwd.xyz>
2023-05-23 22:03:04 +0200

    :green_heart: Fix docker build (#165)
    * :green_heart: Fix docker build
    
    Pass fake env var to get be able to build the image.
    Fly will override it with secrets when actually starting the container
    
    * update

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -2,7 +2,6 @@ import { Chain } from 'wagmi'
 import { sepolia } from 'wagmi/chains'
 
 interface Config {
-  appName: string
   chains: Chain[]
   urls: {
     alchemy: string
@@ -12,25 +11,23 @@ interface Config {
   walletConnectProjectId: string
 }
 
-function validateConfig(config: any): Config {
-  Object.keys(config).forEach((key) => {
-    const value = config[key]
-    if (value === undefined || value === null)
-      console.error(`${key} is not set`)
-  })
-
-  return config
-}
-
 // need to use full reference to process.env
 // can't destructure or do process.env[name]
 // https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#loading-environment-variables
 const alchemyRpcUrl = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
-const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
-const proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL
-const queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL
+if (alchemyRpcUrl === undefined) throw new Error('ALCHEMY_RPC_URL is not set')
 
-const config = {
+const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
+if (walletConnectProjectId === undefined)
+  throw new Error('WALLET_CONNECT_PROJECT_ID is not set')
+
+const proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL
+if (proveApiUrl === undefined) throw new Error('PROVE_API_URL is not set')
+
+const queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL
+if (queryApiUrl === undefined) throw new Error('QUERY_API_URL is not set')
+
+const config: Config = {
   chains: [sepolia],
   urls: {
     alchemy: alchemyRpcUrl,
@@ -40,4 +37,4 @@ const config = {
   walletConnectProjectId,
 }
 
-export default validateConfig(config)
+export default config
