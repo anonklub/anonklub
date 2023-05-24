81e5937794a03aa157641d563656feb5e43fa6b1
3pwd <me@3pwd.xyz>
2023-05-24 09:09:21 +0200

    :bug: Force skipping next cache for fetch requests

--- ui/src/lib/get-data.ts
+++ ui/src/lib/get-data.ts
@@ -1,5 +1,6 @@
 export async function getData<T>(url: string): Promise<T> {
-  const res = await fetch(url)
+  // query API might return different results after every new block (12s), so skip cache
+  const res = await fetch(url, { cache: 'no-store' })
 
   if (!res.ok) throw new Error('Failed to fetch data')
 
