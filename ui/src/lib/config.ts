82c3e3b1acfc490676f0506240f9d9c29bd419d2
Threepwood <me@3pwd.xyz>
2023-05-26 14:26:34 +0200

    :construction_worker: Include build check in GH action (#177)
    * :construction_worker: Include build check in GH action
    
    * :green_heart: Include graph api secret for CI build check
    
    * :green_heart: Set dummy env values for ui in NODE_ENV test env

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -15,18 +15,24 @@ interface Config {
 // need to use full reference to process.env
 // can't destructure or do process.env[name]
 // https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#loading-environment-variables
-const alchemyRpcUrl = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
-if (alchemyRpcUrl === undefined) throw new Error('ALCHEMY_RPC_URL is not set')
+let alchemyRpcUrl = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
+let walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
+let proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL
+let queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL
 
-const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
-if (walletConnectProjectId === undefined)
-  throw new Error('WALLET_CONNECT_PROJECT_ID is not set')
-
-const proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL
-if (proveApiUrl === undefined) throw new Error('PROVE_API_URL is not set')
-
-const queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL
-if (queryApiUrl === undefined) throw new Error('QUERY_API_URL is not set')
+if (process.env.NEXT_PUBLIC_NODE_ENV !== 'test') {
+  if (alchemyRpcUrl === undefined) throw new Error('ALCHEMY_RPC_URL is not set')
+  if (walletConnectProjectId === undefined)
+    throw new Error('WALLET_CONNECT_PROJECT_ID is not set')
+  if (proveApiUrl === undefined) throw new Error('PROVE_API_URL is not set')
+  if (queryApiUrl === undefined) throw new Error('QUERY_API_URL is not set')
+} else {
+  // set dummy values to allow tests and CI build checks to pass
+  alchemyRpcUrl ??= 'http://alchemy.com'
+  walletConnectProjectId ??= 'foo'
+  proveApiUrl ??= 'http://localhost:3000/api/prove'
+  queryApiUrl ??= 'http://localhost:3000/api/query'
+}
 
 export const config: Config = {
   appTitle: 'Anonklub',
