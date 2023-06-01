82086903294e1b4d44d384a64e35ab11a87a824d
Threepwood <me@3pwd.xyz>
2023-06-01 11:49:13 +0200

    :wrench: Update next.config.js (#188)
    * Update next.config.js
    
    * fix: import `groth16` with `require`

--- ui/next.config.js
+++ ui/next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
+  webpack: function (config, options) {
+    if (!options.isServer) {
+      config.resolve.fallback.fs = false
+      config.resolve.fallback.readline = false
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
