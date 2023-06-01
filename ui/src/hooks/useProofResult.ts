361f7a4a5933e023dbf6f6630fd154e93cead7b6
3pwd <me@3pwd.xyz>
2023-06-01 17:35:16 +0200

    Submit proof request to real prove api server
    Remove mock response

--- ui/src/hooks/useProofResult.ts
+++ ui/src/hooks/useProofResult.ts
@@ -10,12 +10,7 @@ export const useProofResult = () => {
     isLoading,
   } = useAsync(async () => {
     if (proofRequest === null) return
-    // TODO: remove, temporary hack to simulate proving server response
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-
-    // const { jobId } = await proofRequest.submit()
-    const jobId =
-      'e3d6e923cc26d886bb9f76413fa0652952f6446ab72430eab82ffe69876043d2'
+    const { jobId } = await proofRequest.submit()
     return jobId
   }, 'submit-proof-request')
 
