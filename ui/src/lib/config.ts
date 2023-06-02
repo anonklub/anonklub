b1118d0363c233f7873eb85c045c31ee160b0b38
3pwd <me@3pwd.xyz>
2023-06-02 12:22:59 +0200

    refactor config

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -13,21 +13,23 @@ interface Config {
 
 // need to use full reference to process.env, can't destructure or do process.env[name]
 // https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#loading-environment-variables
-let walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
-let proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL
-let queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL
+const walletConnectProjectId =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ?? ''
+const proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL ?? ''
+const queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL ?? ''
 
-if (process.env.NEXT_PUBLIC_NODE_ENV !== 'test') {
-  if (walletConnectProjectId === undefined)
-    throw new Error('WALLET_CONNECT_PROJECT_ID is not set')
-  if (proveApiUrl === undefined) throw new Error('PROVE_API_URL is not set')
-  if (queryApiUrl === undefined) throw new Error('QUERY_API_URL is not set')
-} else {
-  // set dummy values to allow tests and CI build checks to pass
-  walletConnectProjectId ??= 'foo'
-  proveApiUrl ??= 'http://localhost:3000/api/prove'
-  queryApiUrl ??= 'http://localhost:3000/api/query'
-}
+;[
+  [walletConnectProjectId, 'NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID'],
+  [proveApiUrl, 'NEXT_PUBLIC_PROVE_API_URL'],
+  [queryApiUrl, 'NEXT_PUBLIC_QUERY_API_URL'],
+].forEach(([value, name]) => {
+  if (value === '') {
+    // this means next build will fail
+    if (process.env.NEXT_PHASE === 'phase-production-build')
+      throw new Error(`Missing environment variable ${name}`)
+    else console.warn(`Missing environment variable ${name}`)
+  }
+})
 
 export const config: Config = {
   appTitle: 'Anonklub',
