a3aa85261a660a387acc574aeb0dd2a9fea67625
Threepwood <me@3pwd.xyz>
2023-06-02 17:05:02 +0200

    :rocket: Update deploy config (#191)
    * Submit proof request to real prove api server
    
    Remove mock response
    
    * remove unused alchemyRpcUrl env var
    
    * refactor config
    
    * Update fly configs
    
    * set fake ui env var in static analysis workflow

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -5,40 +5,36 @@ interface Config {
   appTitle: string
   chains: Chain[]
   urls: {
-    alchemy: string
     proveApi: string
     queryApi: string
   }
   walletConnectProjectId: string
 }
 
-// need to use full reference to process.env
-// can't destructure or do process.env[name]
+// need to use full reference to process.env, can't destructure or do process.env[name]
 // https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#loading-environment-variables
-let alchemyRpcUrl = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
-let walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
-let proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL
-let queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL
+const walletConnectProjectId =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ?? ''
+const proveApiUrl = process.env.NEXT_PUBLIC_PROVE_API_URL ?? ''
+const queryApiUrl = process.env.NEXT_PUBLIC_QUERY_API_URL ?? ''
 
-if (process.env.NEXT_PUBLIC_NODE_ENV !== 'test') {
-  if (alchemyRpcUrl === undefined) throw new Error('ALCHEMY_RPC_URL is not set')
-  if (walletConnectProjectId === undefined)
-    throw new Error('WALLET_CONNECT_PROJECT_ID is not set')
-  if (proveApiUrl === undefined) throw new Error('PROVE_API_URL is not set')
-  if (queryApiUrl === undefined) throw new Error('QUERY_API_URL is not set')
-} else {
-  // set dummy values to allow tests and CI build checks to pass
-  alchemyRpcUrl ??= 'http://alchemy.com'
-  walletConnectProjectId ??= 'foo'
-  proveApiUrl ??= 'http://localhost:3000/api/prove'
-  queryApiUrl ??= 'http://localhost:3000/api/query'
-}
+;[
+  [walletConnectProjectId, 'NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID'],
+  [proveApiUrl, 'NEXT_PUBLIC_PROVE_API_URL'],
+  [queryApiUrl, 'NEXT_PUBLIC_QUERY_API_URL'],
+].forEach(([value, name]) => {
+  if (value === '') {
+    // this means next build will fail
+    if (process.env.NEXT_PHASE === 'phase-production-build')
+      throw new Error(`Missing environment variable ${name}`)
+    else console.warn(`Missing environment variable ${name}`)
+  }
+})
 
 export const config: Config = {
   appTitle: 'Anonklub',
   chains: [sepolia],
   urls: {
-    alchemy: alchemyRpcUrl,
     proveApi: proveApiUrl,
     queryApi: queryApiUrl,
   },
