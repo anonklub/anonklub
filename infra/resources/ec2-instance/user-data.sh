a53c201f1583b198073e349f1049620e05abf2ab
3pwd <me@3pwd.xyz>
2023-06-02 15:55:02 +0200

    fix user-data script

--- infra/resources/ec2-instance/user-data.sh
+++ infra/resources/ec2-instance/user-data.sh
@@ -4,45 +4,46 @@ set -e
 
 function install_node() {
   curl https://get.volta.sh | bash
-  ln -s /.volta/bin/volta /usr/bin/volta
-  volta install node@19
 
-  ln -s /.volta/bin/node /usr/bin/node
-  ln -s /.volta/bin/npm /usr/bin/npm
+  export VOLTA_HOME="$HOME/.volta"
+  export PATH="$VOLTA_HOME/bin:$PATH"
+
+  volta install node@19
   npm i -g pnpm
-  ln -s /.volta/bin/pnpm /usr/bin/pnpm
+  pnpm -v
 }
 
 function install_redis() {
-  sudo apt update
-  sudo apt install redis-server
-  sudo systemctl enable redis-server.service
-  sudo systemctl start redis-server.service
+  apt update
+  apt install -y redis-server
+  systemctl enable redis-server.service
+  systemctl start redis-server.service
 }
 
 function build() {
-  cd /home/ubuntu
+  cd "$install_dir"
   git clone https://github.com/privacy-scaling-explorations/e2e-zk-ecdsa
   cd e2e-zk-ecdsa
-  pnpm --filter @anonset/prove-api... i
 
+  pnpm --filter @anonset/prove-api... i
   pnpm --filter @anonset/prove-api... build
   rm -rf {./,apis/prove,membership}/node_modules
   pnpm --filter @anonset/prove-api... i --prod
 }
 
 function start() {
-  cd /home/ubuntu/apis/proving
-  nohup node dist >>log.txt 2>&1
+  cd "$install_dir"/e2e-zk-ecdsa/apis/prove
+  nohup node dist >> log.txt 2>&1
 }
 
 function main() {
-  # declare constant
+  local install_dir=/home/ubuntu
+  mkdir -p $install_dir
 
-  install_node
   install_redis
+  install_node
   build
-  start
+  start > $install_dir/install.log 2>&1
 }
 
 main "$@"
