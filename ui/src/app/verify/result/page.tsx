5f967a718b5fa107390dc52aa72316f34e1c9442
Threepwood <me@3pwd.xyz>
2023-06-13 15:31:49 +0200

    :sparkles: Use onchain Verifier to verify proods ui-side (#201)
    * :sparkles: Use onchain Verifier to verify proofs ui-side (#200)
    
    * feat: use on chain verifier contract to verify proofs
    
    * update static-analysis.yml
    
    * update pnpm-lock.yaml
    
    * update ui/tsconfig.json
    
    * pin snyk action version used in snyk check worklfow
    
    * format
    
    * simplify .eslintrc.yaml
    
    * fix static-analysis.yml
    
    * fix fly toml config files
    
    Only accepts string build.args
    
    * fix parsing of `hexstring[][]` into `bigints[]][]`

--- ui/src/app/verify/result/page.tsx
+++ ui/src/app/verify/result/page.tsx
@@ -1,27 +1,11 @@
 'use client'
 import Link from 'next/link'
-import { useEffect } from 'react'
-import verificationKey from '$/verification_key.json'
 import { Loader } from '@components'
-import { useAsync, useStore } from '@hooks'
-
-const { groth16 } = require('snarkjs')
+import { useStore, useVerifyProof } from '@hooks'
 
 export default function Page() {
   const { proof, publicSignals } = useStore()
-  const {
-    data: valid,
-    error,
-    execute,
-    isLoading,
-  } = useAsync<boolean>(
-    () => groth16.verify(verificationKey, publicSignals, proof),
-    'zkp-verification-result',
-  )
-
-  useEffect(() => {
-    execute()
-  }, [execute])
+  const { error, isError, isLoading, valid } = useVerifyProof()
 
   if (proof === null || publicSignals === null)
     return (
@@ -34,8 +18,7 @@ export default function Page() {
       </div>
     )
   if (isLoading) return <Loader />
-  if (error !== undefined && error instanceof Error)
-    return <span>{error.message}</span>
+  if (isError && error !== null) return <span>{error.message}</span>
   return (
     valid !== undefined && (
       <div className='flex flex-col space-y-5'>
---
+++ ui/src/hooks/useVerifyProof.ts
@@ -0,0 +1,48 @@
+import { useEffect } from 'react'
+import { GetFunctionArgs } from 'viem'
+import { useContractRead } from 'wagmi'
+import { abi, bigintify, config } from '#'
+import { useAsync, useStore } from '@/hooks'
+
+const { groth16 } = require('snarkjs')
+
+export const useVerifyProof = () => {
+  const { proof, publicSignals } = useStore()
+
+  const {
+    data,
+    error: errorSnarkJs,
+    execute,
+    isLoading: isLoadingSnarkJs,
+  } = useAsync<string>(
+    () => groth16.exportSolidityCallData(proof, publicSignals),
+    'verify-proof-calldata',
+  )
+  const args: GetFunctionArgs<typeof abi, 'verifyProof'>['args'] =
+    data !== undefined ? bigintify(JSON.parse(`[${data}]`)) : undefined
+
+  useEffect(() => {
+    execute()
+  }, [execute])
+
+  const {
+    data: valid,
+    error,
+    isError,
+    isLoading,
+  } = useContractRead({
+    abi,
+    address: config.verifier.address,
+    args,
+    chainId: config.verifier.chainId,
+    enabled: args !== undefined && !isLoadingSnarkJs,
+    functionName: 'verifyProof',
+  })
+
+  return {
+    error: errorSnarkJs ?? error,
+    isError: errorSnarkJs !== undefined || isError,
+    isLoading: isLoadingSnarkJs || isLoading,
+    valid,
+  }
+}
