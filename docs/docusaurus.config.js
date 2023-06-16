3e9969404ee30d3cd4a44e9f7e757d21ff143a16
Threepwood <me@3pwd.xyz>
2023-06-16 09:16:01 +0200

    fix(docs): fix edit links base url (#205)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/privacy-scaling-explorations/tree/main/docs/docs',
+            'https://github.com/privacy-scaling-explorations/e2e-zk-ecdsa/edit/main/docs',
           sidebarPath: require.resolve('./sidebars.js'),
         },
         theme: {
