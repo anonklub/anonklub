dbfc90c3e518ede257f3410ec8685894912a5f95
sripwoud <me@sripwoud.xyz>
2023-08-10 22:09:23 +0200

    Build @anonklub/query to commonJS

--- @anonklub/query/src/index.ts
+++ @anonklub/query/src/index.ts
@@ -1,4 +1,10 @@
 export * from 'AnonymitySet'
 export * from 'CONSTANTS'
-export * from 'requests'
-export { Choice, Environment } from 'types'
+export * from 'requests/index'
+export {
+  AnonSetResponse,
+  Choice,
+  Endpoint,
+  Environment,
+  Request,
+} from 'types'
