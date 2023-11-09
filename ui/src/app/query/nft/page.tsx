acccaa0744dfb7ee8e01804c04f62b59b2e1775f
sripwoud <me@sripwoud.xyz>
2023-11-09 08:43:42 +0100

    add nft query page

---
+++ ui/src/app/query/nft/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { useState } from 'react'
+import { config, getData } from '#'
+import { AnonSetResults, HelpModal, Loader } from '@components'
+import { useAsync, useStore } from '@hooks'
+
+export default function Page() {
+  const [tokenAddress, setTokenAddress] = useState<string>('')
+  const { anonSet, setAnonSet } = useStore()
+  const { error, execute, isLoading } = useAsync(async () => {
+    if (tokenAddress !== '') {
+      const data = await getData<string[]>(
+        `${config.urls.queryApi}/nft?tokenAddress=${tokenAddress}`,
+      )
+      setAnonSet(data)
+    }
+  })
+  const canFetch = tokenAddress !== ''
+
+  if (isLoading) return <Loader />
+  if (error instanceof Error) return <span>Error: {error.message}</span>
+  if (anonSet !== null)
+    return <AnonSetResults anonSet={anonSet} title='ERC20 Balance' />
+
+  return (
+    <div className='flex flex-col items-center space-y-10'>
+      <div className='self-end'>
+        <HelpModal
+          content={[
+            'Provide the ERC20 token address and the minimum amount of tokens one should own to be part of the anonset.',
+          ]}
+        />
+      </div>
+      <div className='nes-field w-[550px]'>
+        <label>
+          ERC20 Token Address
+          <input
+            placeholder='0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72'
+            type='text'
+            className='nes-input text-xs'
+            value={tokenAddress}
+            onChange={({ target }) => {
+              setTokenAddress(target.value)
+            }}
+          />
+        </label>
+      </div>
+      <button
+        type='button'
+        className={`nes-btn self-center ${
+          canFetch ? 'is-warning' : 'is-disabled'
+        }`}
+        onClick={execute}
+      >
+        Fetch Anonset
+      </button>
+    </div>
+  )
+}
