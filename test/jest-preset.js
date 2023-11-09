ebfccda081c2c7e02e562d60eccc3b1efe8e4896
sripwoud <me@sripwoud.xyz>
2023-11-09 09:39:45 +0100

    decrease global branches coverage threshold

--- test/jest-preset.js
+++ test/jest-preset.js
@@ -17,7 +17,7 @@ module.exports = {
   coverageDirectory: join(__dirname, '..', 'coverage'),
   coverageThreshold: {
     global: {
-      branches: 48,
+      branches: 46,
       functions: 70,
       lines: 80,
       statements: 80,
