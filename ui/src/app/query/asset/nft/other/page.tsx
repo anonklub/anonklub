a85912fe0cbc9a28c118952e91485179eb2a68e8
sripwoud <me@sripwoud.xyz>
2023-11-09 09:53:47 +0100

    :sparkles: Gen proofs for nft collection owners anonsets (#247)
    * fix ui eslint config
    
    * add nft query page
    
    * :sparkles: Can generate proof for nft owners anonsets
    
    * format
    
    * bump turbo
    
    * remove witness.wtns link
    
    This is not served by prove api
    
    * decrease global branches coverage threshold
    
    * decrease global functions coverage threshold

---
+++ ui/src/app/query/asset/nft/other/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { useState } from 'react'
+import { config, getData } from '#'
+import { AnonSetResults, HelpModal, Loader } from '@components'
+import { useAsync, useStore } from '@hooks'
+
+export default function Page() {
+  const [tokenAddress, setTokenAddress] = useState<string>('')
+  const { anonSet, setAnonSet } = useStore()
+  const { error, execute, isLoading } = useAsync(async () => {
+    if (tokenAddress !== '') {
+      const data = await getData<string[]>(
+        `${config.urls.queryApi}/nft?tokenAddress=${tokenAddress}`,
+      )
+      setAnonSet(data)
+    }
+  })
+  const canFetch = tokenAddress !== ''
+
+  if (isLoading) return <Loader />
+  if (error instanceof Error) return <span>Error: {error.message}</span>
+  if (anonSet !== null)
+    return <AnonSetResults anonSet={anonSet} title='NFT Owners' />
+
+  return (
+    <div className='flex flex-col items-center space-y-10'>
+      <div className='self-end'>
+        <HelpModal content={['Provide the NFT contract address.']} />
+      </div>
+      <div className='nes-field w-[550px]'>
+        <label>
+          NFT Contract Address
+          <input
+            placeholder='0x99a9b7c1116f9ceeb1652de04d5969cce509b069'
+            type='text'
+            className='nes-input text-xs'
+            value={tokenAddress}
+            onChange={({ target }) => {
+              setTokenAddress(target.value)
+            }}
+          />
+        </label>
+      </div>
+      <button
+        type='button'
+        className={`nes-btn self-center ${
+          canFetch ? 'is-warning' : 'is-disabled'
+        }`}
+        onClick={execute}
+      >
+        Fetch Anonset
+      </button>
+    </div>
+  )
+}
