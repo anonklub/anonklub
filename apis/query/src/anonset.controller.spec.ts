1a6e2777a80d5edcf14a1ed4b25d429e0a7ea5ae
sripwoud <me@sripwoud.xyz>
2023-11-10 19:03:54 +0100

    remove nest test
    TODO

--- apis/query/src/anonset.controller.spec.ts
+++ apis/query/src/anonset.controller.spec.ts
@@ -1,22 +1,3 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { AnonsetController } from './anonset.controller'
-import { AnonsetService } from './anonset.service'
-
 describe('AnonsetController', () => {
-  let AnonsetController: AnonsetController
-
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AnonsetController],
-      providers: [AnonsetService],
-    }).compile()
-
-    AnonsetController = app.get<AnonsetController>(AnonsetController)
-  })
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(AnonsetController.getHello()).toBe('Hello World!')
-    })
-  })
+  it.todo('todo')
 })
