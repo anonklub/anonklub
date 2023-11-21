abf1a650782a8051455089d65a54c093afe07891
sripwoud <me@sripwoud.xyz>
2023-11-21 08:56:42 +0300

    :sparkles:(ui): can fetch anonset for beacon depositors

--- ui/src/app/prove/prepare/from-on-chain/page.tsx
+++ ui/src/app/prove/prepare/from-on-chain/page.tsx
@@ -11,10 +11,12 @@ export default function ChooseAnonSetTypePage() {
       help={[
         'Asset: are you a member of the group of people who own some ETH, ERC20, NFTs...?',
         'DAO: are you a member of the group of people involved in a DAO governance?',
+        'Beacon: are you a member of the group of addresses who have deposited ETH in the Ethereum 2.0 beacon contract (0x00000000219ab540356cbb839cbe05303d7705fa)?',
       ]}
       buttons={[
         { href: '/prove/prepare/from-on-chain/asset', text: 'Asset' },
         { href: '/prove/prepare/from-on-chain/dao', text: 'DAO' },
+        { href: '/query/beacon', text: 'Beacon' },
       ]}
     />
   )
---
+++ ui/src/app/query/beacon/loading.tsx
@@ -0,0 +1,3 @@
+import { Loader } from '@components'
+
+export default Loader
