5fd0ba5f92687946044b497838ca672f9e211fb9
isk <iskander.s.andrews@gmail.com>
2024-01-16 17:46:28 +0100

    feat: implementing a wasm-pack liberary for merkle tree

---
+++ merkle_tree_wasm/wasm-pkg/README.md
@@ -0,0 +1,3 @@
+# @anonklub/merkle-tree-wasm
+
+A Wasm TS generated package from the `merkle_tree_wasm` crate to be integrated in the client-side.
---
+++ merkle_tree_wasm/wasm-pkg/index.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {(string)[]} leaves
+* @param {string} leaf
+* @param {number} depth
+* @returns {Uint8Array}
+*/
+export function generate_merkle_proof(leaves: (string)[], leaf: string, depth: number): Uint8Array;
