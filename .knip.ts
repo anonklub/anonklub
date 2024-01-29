18d59ca6f9ee7c890fae0fa26535740fc68b5656
sripwoud <me@sripwoud.xyz>
2024-01-29 14:10:15 +0100

    move `apis/query` into `query-api`

--- .knip.ts
+++ .knip.ts
@@ -3,11 +3,11 @@ import type { KnipConfig } from 'knip'
 const config: KnipConfig = {
   ignore: [
     'apis/prove/src/mq/processor.ts',
-    'apis/query/src/api/controllers/AnonymitySet.ts',
-    'apis/query/src/api/controllers/requests/**',
-    'apis/query/src/api/middlewares/**',
-    'apis/query/src/api/repositories/**',
-    'apis/query/src/lib/graph/.graphclient/**',
+    'query-api/src/api/controllers/AnonymitySet.ts',
+    'query-api/src/api/controllers/requests/**',
+    'query-api/src/api/middlewares/**',
+    'query-api/src/api/repositories/**',
+    'query-api/src/lib/graph/.graphclient/**',
     'circuits/circom/generated/**',
     'contracts/lib/openzeppelin-contracts/**',
     'discord-bot/src/lib/logger.ts',
