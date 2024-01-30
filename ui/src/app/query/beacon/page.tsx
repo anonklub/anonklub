62b903615db51b0100a052fe26c081bfa1592206
sripwoud <me@sripwoud.xyz>
2024-01-30 16:09:13 +0100

    refactor: delete proving server related code (#316)
    * bump lock file
    
    * delete `/apis/prove`
    
    * move `/apis/query/` into `query-api/`
    
    * remove `/infra`
    
    * update config files
    
    * format
    
    * remove @anonklub/proof ts reference from circom pkg
    
    * tsignore
    
    * pin `@anonklub/proof` package in ui
    
    * remove unused config in hook
    
    * tsignore (discord bot)
    
    * delete apis/prove
    
    * remove tsignore directive
    
    * update env vars
    
    * skip build of `@anonklub/proof` in vlaidate workflow
    
    version was pinned in UI and CLI
    
    * avoid pre rendering for beacon query page
    
     see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
    
    By default, next will try to pre-render and fetch the data at build time
    However, this is a dune query that is subject to query credits... sometimes the query fails. It should not prevent building the app though.
    
    * Remove prove api url from ui config

--- ui/src/app/query/beacon/page.tsx
+++ ui/src/app/query/beacon/page.tsx
@@ -3,6 +3,11 @@ import { config } from '#/config'
 import { getData } from '#/get-data'
 import { AnonSetResults } from '@components'
 
+// see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+// By default, next will try to pre-render and fetch the data at build time
+// However, this is a dune query that is subject to query credits... sometimes the query fails. It should not prevent building the app though.
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
   const anonSet = await getData<string[]>(`${config.urls.queryApi}/beacon`)
 
