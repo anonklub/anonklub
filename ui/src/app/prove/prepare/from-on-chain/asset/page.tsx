bc3022254c33010daa277e2317af3b966b7ad4ca
sripwoud <me@sripwoud.xyz>
2024-02-01 21:50:56 +0100

    feat: add eth balance query screen (#338)

--- ui/src/app/prove/prepare/from-on-chain/asset/page.tsx
+++ ui/src/app/prove/prepare/from-on-chain/asset/page.tsx
@@ -9,12 +9,14 @@ export default function ChooseAnonSetAssetPage() {
     <Screen
       question='What type of asset do you want to prove you own?'
       help={[
-        'NFT: are you a member of the group of people who own an NFT of a given collection?',
         'ERC20: are you a member of the group of people who own a min amount of a given ERC20 token?',
+        'ETH: are you a member of the group of people who own a min amount of ETH?',
+        'NFT: are you a member of the group of people who own an NFT of a given collection?',
       ]}
       buttons={[
-        { href: '/prove/prepare/from-on-chain/asset/nft', text: 'NFT' },
         { href: '/query/asset/erc20', text: 'ERC20' },
+        { href: '/query/asset/eth', text: 'ETH' },
+        { href: '/prove/prepare/from-on-chain/asset/nft', text: 'NFT' },
       ]}
     />
   )
