7ce6c3e161d059cf03add88f5d208e769eaf0d96
sripwoud <me@sripwoud.xyz>
2024-02-01 20:57:58 +0100

    feat: show addresses count in `ScrollableJsonContainer.tsx`

--- ui/src/components/ScrollableJsonContainer.tsx
+++ ui/src/components/ScrollableJsonContainer.tsx
@@ -2,8 +2,15 @@ import { JSONValue } from '@components'
 
 export function ScrollableJsonContainer({ data }: { data: JSONValue }) {
   return (
-    <div className='nes-container is-rounded flex max-h-[325px] overflow-auto scroll-smooth'>
-      {JSON.stringify(data, null, 2)}
-    </div>
+    <>
+      {Array.isArray(data) && (
+        <span className='nes-text is-primary text-xl'>
+          Count: {data.length}
+        </span>
+      )}
+      <div className='nes-container is-rounded flex max-h-[325px] overflow-auto scroll-smooth'>
+        {JSON.stringify(data, null, 2)}
+      </div>
+    </>
   )
 }
