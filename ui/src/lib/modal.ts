6570b8a08a510b08a8ed08940cc23c02b9dcc8bb
sripwoud <me@sripwoud.xyz>
2024-02-02 22:26:27 +0100

    feat: close help modal on click outside event

--- ui/src/lib/modal.ts
+++ ui/src/lib/modal.ts
@@ -2,11 +2,24 @@ import { RefObject } from 'react'
 
 export const modal = (ref: RefObject<HTMLDialogElement>) => {
   const open = () => {
-    if (ref.current?.open === false) ref.current.showModal()
+    if (ref.current?.open === false) {
+      ref.current.showModal()
+      // Attach the event listener to handle outside clicks
+      ref.current.addEventListener('click', handleOutsideClick)
+    }
   }
 
   const close = () => {
-    if (ref.current?.open === true) ref.current?.close()
+    if (ref.current?.open === true) {
+      ref.current?.close()
+      // Remove the event listener when the modal is closed
+      ref.current.removeEventListener('click', handleOutsideClick)
+    }
+  }
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    // Close the modal if the click is on the dialog element itself
+    if (event.target === ref.current) close()
   }
 
   return {
