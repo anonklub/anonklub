73aa03901afa8c4eca227d1797b971c7bbf17d5d
sripwoud <me@sripwoud.xyz>
2024-02-02 16:25:51 +0100

    replace star icon by check mark font awesome svg

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -5,13 +5,13 @@ export function Footer() {
   return (
     <section className='icon-list flex flex-row items-center justify-center space-x-2'>
       <ExternalLink href={Links.Twitter} className='footer-link'>
-        <Image src='twitter.svg' width={25} height={25} alt='twitter icon' />
+        <Image src='/twitter.svg' width={25} height={25} alt='twitter icon' />
       </ExternalLink>
       <ExternalLink href={Links.Docs} className='footer-link'>
         Docs
       </ExternalLink>
       <ExternalLink href={Links.Github} className='footer-link'>
-        <Image src='github.svg' width={25} height={25} alt='github icon' />
+        <Image src='/github.svg' width={25} height={25} alt='github icon' />
       </ExternalLink>
     </section>
   )
