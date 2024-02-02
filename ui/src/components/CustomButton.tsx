fe3876dbdb1b8d67ce8d0386cfcf69e8d66276a2
sripwoud <me@sripwoud.xyz>
2024-02-02 22:28:52 +0100

    fix styles and hydration error (#346)
    * fix: use background image as png instead of svg
    
    * fix: use background image as png instead of svg
    
    * make logo size responsive
    
    * fix web3 connect button hydration error
    
    * revert colors
    
    Avoid red on red / blue on blue
    
    * feat: close help modal on click outside event
    
    * update cancel button style

--- ui/src/components/CustomButton.tsx
+++ ui/src/components/CustomButton.tsx
@@ -1,14 +1,23 @@
 'use client'
 import { useWeb3Modal } from '@web3modal/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useAccount, useDisconnect } from 'wagmi'
+import { Loader } from '@/components/Loader'
 
 export function CustomButton() {
+  // to avoid hydration errors
+  // (content rendered by React on the client-side doesn't match the HTML sent from the server)
+  // here label depends on the client side state which is known only after the component is mounted
+  const [hasMounted, setHasMounted] = useState(false)
   const [loading, setLoading] = useState(false)
   const { open } = useWeb3Modal()
   const { isConnected } = useAccount()
   const { disconnect } = useDisconnect()
   const label = isConnected ? 'Disconnect' : 'Connect'
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
 
   async function onOpen() {
     setLoading(true)
@@ -24,9 +33,11 @@ export function CustomButton() {
     }
   }
 
-  return (
+  return hasMounted ? (
     <button className='btn btn-connect' onClick={onClick} disabled={loading}>
       {loading ? 'Loading...' : label}
     </button>
+  ) : (
+    Loader
   )
 }
