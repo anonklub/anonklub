df1f89ade95ba44c68534d8fec54af16a3c2ab37
sripwoud <me@sripwoud.xyz>
2024-02-08 14:06:07 +0100

    chore: Add comments

--- ui/src/app/prove/result/page.tsx
+++ ui/src/app/prove/result/page.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { Loader } from '@components'
 import { useProofResult } from '@hooks'
 
+// TODO: extract in /lib
 const ellipsify = (text: string, start = 6): string => {
   if (text.length <= start) {
     return text
@@ -39,6 +40,7 @@ export default function Page() {
     <div className='flex flex-col items-center justify-center space-y-10'>
       <h2 className='self-start'>Proof Results</h2>
 
+      {/* TODO: change color, red on red is hard to read */}
       <div className='flex flex-col text-red'>
         {ellipsify(fullProof.toString(), 100)}
       </div>
