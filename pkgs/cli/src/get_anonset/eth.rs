3ee7fa0e2eea7dfd8465832ca6b1062e4c260fd8
sripwoud <me@sripwoud.xyz>
2024-03-01 14:27:41 +0100

    refactor

--- pkgs/cli/src/get_anonset/eth.rs
+++ pkgs/cli/src/get_anonset/eth.rs
@@ -1,23 +1,33 @@
-use crate::get_query_client;
+use crate::{get_query_client, Anonset};
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
-
 
 #[derive(Serialize, Deserialize)]
 struct EthAnonSetQuery {
     min: Option<u64>,
 }
 
-pub async fn get_eth_anonset(min: Option<u64>) -> Result<Vec<String>> {
+pub async fn get_eth_anonset(min: Option<u64>) -> Result<Anonset> {
     let eth_anonset_query = EthAnonSetQuery { min };
-    let client = get_query_client()?;
-    let req = client.get("/asset/eth").query(&eth_anonset_query).map_err(|e| anyhow::anyhow!("Error building request: {:?}", e))?; // .await?;
-    let mut res = req.send().await.map_err(|e| anyhow::anyhow!("Error sending request: {:?}", e))?;
+
+    let mut res = get_query_client()?
+        .get("/asset/eth")
+        .query(&eth_anonset_query)
+        .map_err(|e| anyhow::anyhow!("Error building request: {:?}", e))?
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!("Error sending request: {:?}", e))?;
 
     if res.status().is_success() {
-        let anonset: Vec<String> = res.body_json().await.map_err(|e| anyhow::anyhow!("Error parsing response: {:?}", e))?;
-        Ok(anonset)
+        let anonset: Vec<String> = res
+            .body_json()
+            .await
+            .map_err(|e| anyhow::anyhow!("Error parsing response: {:?}", e))?;
+        Ok(Anonset(anonset))
     } else {
-        Err(anyhow::anyhow!("Request failed with status: {:?}", res.status()))
+        Err(anyhow::anyhow!(
+            "Request failed with status: {:?}",
+            res.status()
+        ))
     }
 }
