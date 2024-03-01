e45f107b71e4e1a437bdadd557c9da484ee59a11
sripwoud <me@sripwoud.xyz>
2024-03-01 14:49:46 +0100

    refactor: define generic get_anonset function

--- pkgs/cli/src/get_anonset.rs
+++ pkgs/cli/src/get_anonset.rs
@@ -1,4 +1,5 @@
 mod beacon;
+use anyhow::{anyhow, Result};
 pub use beacon::*;
 
 mod ens_dao;
@@ -17,5 +18,27 @@ pub mod punks;
 pub use punks::*;
 use serde::{Deserialize, Serialize};
 
+use crate::get_query_client;
+
 #[derive(Serialize, Deserialize)]
 pub struct Anonset(pub Vec<String>);
+
+pub async fn get_anonset<T:Serialize>(query_params: T, endpoint: &str) -> Result<Anonset> {
+    let mut res = get_query_client()?
+        .get(endpoint)
+        .query(&query_params)
+        .map_err(|e| anyhow!("Error building request: {:?}", e))?
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!("Error fetching anonset: {:?}", e))?;
+
+    if res.status().is_success() {
+        let anonset = res
+            .body_json()
+            .await
+            .map_err(|e| anyhow::anyhow!("Error parsing anonset: {:?}", e))?;
+        Ok(Anonset(anonset))
+    } else {
+        Err(anyhow::anyhow!("Request failed with status: {:?}", res.status()))
+    }
+}
--- pkgs/cli/src/get_anonset/eth.rs
+++ pkgs/cli/src/get_anonset/eth.rs
@@ -1,4 +1,4 @@
-use crate::{get_query_client, Anonset};
+use crate::{get_anonset, Anonset};
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
@@ -8,26 +8,5 @@ struct EthAnonSetQuery {
 }
 
 pub async fn get_eth_anonset(min: Option<u64>) -> Result<Anonset> {
-    let eth_anonset_query = EthAnonSetQuery { min };
-
-    let mut res = get_query_client()?
-        .get("/asset/eth")
-        .query(&eth_anonset_query)
-        .map_err(|e| anyhow::anyhow!("Error building request: {:?}", e))?
-        .send()
-        .await
-        .map_err(|e| anyhow::anyhow!("Error sending request: {:?}", e))?;
-
-    if res.status().is_success() {
-        let anonset: Vec<String> = res
-            .body_json()
-            .await
-            .map_err(|e| anyhow::anyhow!("Error parsing response: {:?}", e))?;
-        Ok(Anonset(anonset))
-    } else {
-        Err(anyhow::anyhow!(
-            "Request failed with status: {:?}",
-            res.status()
-        ))
-    }
+    get_anonset(EthAnonSetQuery { min }, "/eth/anonset").await
 }
