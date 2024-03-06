61ee020312aee1b8c866ccc2fbd097578584b8c3
isk <0xisk@proton.me>
2024-03-06 16:31:43 +0100

    fix: remove wrong js compiles scripts

--- pkgs/spartan-ecdsa-worker/src/index.js
+++
@@ -1,6 +0,0 @@
-import { wrap } from 'comlink';
-let SpartanEcdsaWorker;
-if (typeof window !== 'undefined') {
-    SpartanEcdsaWorker = wrap(new Worker(new URL('./worker.js', import.meta.url)));
-}
-export { SpartanEcdsaWorker };
