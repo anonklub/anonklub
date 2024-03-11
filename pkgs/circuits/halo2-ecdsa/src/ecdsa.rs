f50031fcfe6b204b333d2aff8309db128915b0f4
0xisk <0xisk@proton.me>
2024-03-11 22:00:37 +0100

    feat: adding halo2wrong ecdsa verison circuit wip

---
+++ pkgs/circuits/halo2-ecdsa/src/ecdsa.rs
@@ -0,0 +1,99 @@
+use ecc::maingate::RegionCtx;
+use ecc::{AssignedPoint, EccConfig, GeneralEccChip};
+use halo2::arithmetic::CurveAffine;
+use halo2::plonk::Error;
+use halo2wrong::curves::ff::PrimeField;
+use integer::rns::Integer;
+use integer::{AssignedInteger, IntegerChip, IntegerConfig,};
+use maingate::{MainGateConfig, RangeConfig};
+
+#[derive(Debug, Clone)]
+pub struct EcdsaConfig {
+    pub main_gate_config: MainGateConfig,
+    pub range_config: RangeConfig,
+}
+
+impl EcdsaConfig {
+    pub fn new(main_gate_config: MainGateConfig, range_config: RangeConfig) -> Self {
+        Self {
+            main_gate_config,
+            range_config,
+        }
+    }
+
+    pub fn ecc_chip_config(&self) -> EccConfig {
+        EccConfig::new(self.range_config.clone(), self.main_gate_config.clone())
+    }
+
+    pub fn integer_chip_config(&self) -> IntegerConfig {
+        IntegerConfig::new(self.range_config.clone(), self.main_gate_config.clone())
+    }
+}
+
+pub struct EcdsaSig<
+    W: PrimeField,
+    N: PrimeField,
+    const NUMBER_OF_LIMBS: usize,
+    const BIT_LEN_LIMB: usize,
+> {
+    pub r: Integer<W, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+    pub s: Integer<W, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+}
+
+pub struct AssignedEcdsaSig<
+    W: PrimeField,
+    N: PrimeField,
+    const NUMBER_OF_LIMBS: usize,
+    const BIT_LEN_LIMB: usize,
+> {
+    pub r: AssignedInteger<W, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+    pub s: AssignedInteger<W, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+}
+
+pub struct AssignedPublicKey<
+    W: PrimeField,
+    N: PrimeField,
+    const NUMBER_OF_LIMBS: usize,
+    const BIT_LEN_LIMB: usize,
+> {
+    pub point: AssignedPoint<W, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+}
+
+pub struct EcdsaChip<
+    E: CurveAffine,
+    N: PrimeField,
+    const NUMBER_OF_LIMBS: usize,
+    const BIT_LEN_LIMB: usize,
+>(GeneralEccChip<E, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>);
+
+impl<E: CurveAffine, N: PrimeField, const NUMBER_OF_LIMBS: usize, const BIT_LEN_LIMB: usize>
+    EcdsaChip<E, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>
+{
+    pub fn new(ecc_chip: GeneralEccChip<E, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>) -> Self {
+        Self(ecc_chip)
+    }
+
+    pub fn scalar_filed_chip(
+        &self,
+    ) -> &IntegerChip<E::ScalarExt, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB> {
+        self.scalar_filed_chip()
+    }
+
+    fn ecc_clip(&self) -> GeneralEccChip<E, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB> {
+        self.clone()
+    }
+}
+
+impl<E: CurveAffine, N: PrimeField, const NUMBER_OF_LIMBS: usize, const BIT_LEN_LIMB: usize>
+    EcdsaChip<E, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>
+{
+    pub fn verify(
+        &self,
+        ctx: &mut RegionCtx<'_, N>,
+        sig: &AssignedEcdsaSig<E::Scalar, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+        pk: &AssignedPublicKey<E::Base, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+        msg_hash: &AssignedInteger<E::Scalar, N, NUMBER_OF_LIMBS, BIT_LEN_LIMB>,
+    ) -> Result<(), Error> {
+        Ok(false)
+    }
+}
