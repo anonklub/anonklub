5d84056f799ced1f539d50fe1a74c6cc3b3e7472
0xisk <0xisk@proton.me>
2024-03-18 10:29:24 +0100

    feat: ecdsa wip

--- pkgs/circuits/halo2-ecdsa/src/utils.rs
+++ pkgs/circuits/halo2-ecdsa/src/utils.rs
@@ -3,22 +3,27 @@ use std::marker::PhantomData;
 use halo2_proofs::{
     arithmetic::CurveAffine,
     circuit::Value,
-    dev::CircuitLayout,
+    dev::{CircuitLayout, MockProver},
     halo2curves::ff::PrimeField,
-    plonk::{keygen_pk, permutation::VerifyingKey, ProvingKey},
-    poly::commitment::Params,
+    plonk::{keygen_pk, keygen_vk, permutation::VerifyingKey, ProvingKey},
+    poly::commitment::{Params, ParamsProver},
 };
-use halo2wrong::curves::secp256k1::Secp256k1Affine;
+use halo2wrong::curves::{ff::FromUniformBytes, pasta::Fp, secp256k1::Secp256k1Affine};
+use maingate::mock_prover_verify;
 use plotters::{backend::BitMapBackend, drawing::IntoDrawingArea, style::WHITE};
 
 use crate::circuit::EcdsaVerifyCircuit;
 
-trait EcdsaVerifyCircuitUtils<E, N> where E: CurveAffine, N: PrimeField {
+trait EcdsaVerifyCircuitUtils<E, N>
+where
+    E: CurveAffine,
+    N: PrimeField,
+{
     // Generates an empty circuit. Useful for generating the proving/verifying keys.
-     fn empty_circuit(aux_generator: E, window_size: usize) -> Self;
+    fn empty_circuit(aux_generator: E, window_size: usize) -> Self;
 
     // Creates a circuit from public key, signature, and message hash
-     fn create_circuit(
+    fn create_circuit(
         public_key: E,
         signature: (E::Scalar, E::Scalar),
         msg_hash: E::Scalar,
@@ -27,19 +32,19 @@ trait EcdsaVerifyCircuitUtils<E, N> where E: CurveAffine, N: PrimeField {
     ) -> Self;
 
     // Draws the layout of the circuit. It is useful for debugging.
-     fn draw_circuit(k: u32, circuit: &Self);
+    fn draw_circuit(k: u32, circuit: &Self);
 
-    // Generates setup parameters using k, which is the number of rows of the circuit
-    // can fit in and must be a power of two
-     fn generate_setup_params(k: u32) -> dyn Params<'static, E>;
-
-    // Generates the verifying and proving keys. We can pass in an empty circuit to generate these
-     fn generate_keys(params: &Params<E>, circuit: &self) -> (ProvingKey<E>, VerifyingKey<E>);
+    // Runs the mock prover and prints any errors
+    fn run_mock_prover(circuit: &Self, pub_input: &Vec<Vec<N>>);
 }
 
-impl<E, N> EcdsaVerifyCircuitUtils<E, N> for EcdsaVerifyCircuit<E, N> where E: CurveAffine, N: PrimeField {
+impl<E, N> EcdsaVerifyCircuitUtils<E, N> for EcdsaVerifyCircuit<E, N>
+where
+    E: CurveAffine,
+    N: PrimeField + FromUniformBytes<64> + Ord,
+{
     // Generates an empty circuit. Useful for generating the proving/verifying keys.
-     fn empty_circuit(aux_generator: E, window_size: usize) -> Self {
+    fn empty_circuit(aux_generator: E, window_size: usize) -> Self {
         Self {
             public_key: Value::unknown(),
             signature: Value::unknown(),
@@ -51,7 +56,7 @@ impl<E, N> EcdsaVerifyCircuitUtils<E, N> for EcdsaVerifyCircuit<E, N> where E: C
     }
 
     // Creates a circuit from public key, signature, and message hash
-     fn create_circuit(
+    fn create_circuit(
         public_key: E,
         signature: (E::Scalar, E::Scalar),
         msg_hash: E::Scalar,
@@ -69,7 +74,7 @@ impl<E, N> EcdsaVerifyCircuitUtils<E, N> for EcdsaVerifyCircuit<E, N> where E: C
     }
 
     // Draws the layout of the circuit. It is useful for debugging.
-     fn draw_circuit(k: u32, circuit: &Self) {
+    fn draw_circuit(k: u32, circuit: &Self) {
         let base = BitMapBackend::new("layout.png", (1600, 1600)).into_drawing_area();
         base.fill(&WHITE).unwrap();
         let base = base
@@ -82,17 +87,30 @@ impl<E, N> EcdsaVerifyCircuitUtils<E, N> for EcdsaVerifyCircuit<E, N> where E: C
             .unwrap();
     }
 
-    // Generates setup parameters using k, which is the number of rows of the circuit
-    // can fit in and must be a power of two
-     fn generate_setup_params(k: u32) -> Params<dyn E> {
-        Params::<E>::new(k)
+    // Runs the mock prover and prints any errors
+    fn run_mock_prover(circuit: &Self, pub_input: &Vec<Vec<N>>) {
+        mock_prover_verify(circuit, pub_input.to_vec())
+    }
+}
+
+trait EcdsaVerifyCircuitSecp256k1 {
+    type param: ParamsProver<Secp256k1Affine>;
+    // Defines the function for generating setup parameters specific to Secp256k1Affine
+    fn generate_setup_params(k: u32) -> Self::param;
+
+    // Defines the function for generating the verifying and proving keys specific to Secp256k1Affine
+    fn generate_keys(circuit: &Self) -> (ProvingKey<Secp256k1Affine>, VerifyingKey<Secp256k1Affine>);
+}
+
+impl EcdsaVerifyCircuitSecp256k1 for EcdsaVerifyCircuit<Secp256k1Affine, Fp> {
+    fn generate_setup_params(k: u32) ->  Self::ParamsType {
+        Params::<Secp256k1Affine>::new(k)
     }
 
-    // Generates the verifying and proving keys. We can pass in an empty circuit to generate these
-     fn generate_keys(params: &Params<E>, circuit: &self) -> (ProvingKey<E>, VerifyingKey<E>) {
-        // just to emphasize that for vk, pk we don't need to know the value of `x`
-        let vk = keygen_pk(params, circuit).expect("vk should not fail");
-        let pk = keygen_pk(params, vk.clone(), circuit).expect("pk should not fail");
+    fn generate_keys(circuit: &Self) -> (ProvingKey<Secp256k1Affine>, VerifyingKey<Secp256k1Affine>) {
+        let params = Self::generate_setup_params(k); // Assuming `k` is available or passed differently
+        let vk = keygen_vk(&params, circuit).expect("vk should not fail");
+        let pk = keygen_pk(&params, vk.clone(), circuit).expect("pk should not fail");
         (pk, vk)
     }
 }
