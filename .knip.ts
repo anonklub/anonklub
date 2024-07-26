2c01b3ec80fd62f59e5aa6aa45fac1ac86c7a29b
sripwoud <me@sripwoud.xyz>
2024-07-26 17:59:07 +0200

    chore: use `lefthook` to manage git hooks (#458)
    * chore: hide biome config file
    
    * chore: update dprint and biome config
    
    * chore: replace lintstaged and husky by lefthook
    
    * chore: update knip config
    
    * chore: cleanup config files in `ui`
    
    * fix: use dprint action in format workflow step

--- .knip.ts
+++ .knip.ts
@@ -5,11 +5,7 @@ const config: KnipConfig = {
     'circom/generated/**',
     'contracts/lib/openzeppelin-contracts/**',
     'discord-bot/src/lib/logger.ts',
-    'query-api/src/api/controllers/AnonymitySet.ts',
-    'query-api/src/api/controllers/requests/**',
-    'query-api/src/api/middlewares/**',
-    'query-api/src/api/repositories/**',
-    'query-api/src/lib/graph/.graphclient/**',
+    'query-api/src/repositories/graph-client/.graphclient/**',
     'ui/public/**',
   ],
 }
