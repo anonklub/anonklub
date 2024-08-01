adaecd0e9bef46587214ed08db1d5e1bafb9571a
sripwoud <me@sripwoud.xyz>
2024-08-01 12:27:35 +0200

    refactor: use verifier object to load wasm async

--- ui/src/app/api/verify/route.ts
+++ ui/src/app/api/verify/route.ts
@@ -1,22 +1,13 @@
-import * as spartanEcdsaWasm from '@anonklub/spartan-ecdsa-wasm'
+import { verifier } from '#/verifier'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
-
-let initialized = false
-
-function maybePrepare() {
-  spartanEcdsaWasm.init_panic_hook()
-  if (initialized) return
-  spartanEcdsaWasm.prepare()
-  initialized = true
-}
 
 export async function POST(request: NextRequest) {
   // TODO: add origin check in a middleware
   // const origin = request.headers.get('origin')
   // if (origin !== config.discordUrl) return NextResponse.error()
-  maybePrepare()
   const proof = new Uint8Array(await request.arrayBuffer())
-  return NextResponse.json(spartanEcdsaWasm.verify_membership(proof))
+  const result = await verifier.verifyMembership(proof)
+  return NextResponse.json(result)
 }
