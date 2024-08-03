55e4d2e3e249c5ed1355b760245529c5d3434df3
sripwoud <me@sripwoud.xyz>
2024-08-03 21:15:42 +0200

    chore: add TODO comment

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
+  // TODO: instead on soft linking files with a script and Dockerfile, do like https://github.com/chungwu/combat-lander/commit/a22a0d2b302b623696b3e95d85566d3a7f1f6e3b
   webpack: (config, options) => {
     if (!options.isServer) {
       config.resolve.fallback.fs = false
