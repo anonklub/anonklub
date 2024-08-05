a6036c85056c3530493ff1bd1cc0de03419163d0
sripwoud <me@sripwoud.xyz>
2024-08-05 16:25:37 +0200

    dev sripwoud (#472)
    * fix(ui): check env var def depending client/server side
    
    * refactor(ui): remove `force-dynamic` for server route
    
    * chore(ui): remove TODO
    
    * chore(ui): use `next/legacy/image`
    
    * fix(ui): force asset/nft/punk page to be dynamic
    
    * chore(ui): custom next config instead of soft linking wasm

--- ui/src/app/api/verify/route.ts
+++ ui/src/app/api/verify/route.ts
@@ -1,12 +1,7 @@
 import { verifier } from '#/verifier'
 import { type NextRequest, NextResponse } from 'next/server'
 
-export const dynamic = 'force-dynamic'
-
 export async function POST(request: NextRequest) {
-  // TODO: add origin check in a middleware
-  // const origin = request.headers.get('origin')
-  // if (origin !== config.discordUrl) return NextResponse.error()
   const proof = new Uint8Array(await request.arrayBuffer())
   const result = await verifier.verifyMembership(proof)
   return NextResponse.json(result)
