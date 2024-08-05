8e164cfb1c7a52b8656dcbad27cb097ce9a9df41
sripwoud <me@sripwoud.xyz>
2024-08-05 12:36:47 +0200

    chore(ui): remove TODO

--- ui/src/app/api/verify/route.ts
+++ ui/src/app/api/verify/route.ts
@@ -2,9 +2,6 @@ import { verifier } from '#/verifier'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
-  // TODO: add origin check in a middleware
-  // const origin = request.headers.get('origin')
-  // if (origin !== config.discordUrl) return NextResponse.error()
   const proof = new Uint8Array(await request.arrayBuffer())
   const result = await verifier.verifyMembership(proof)
   return NextResponse.json(result)
