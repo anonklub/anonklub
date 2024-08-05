48ab9424ba56d28ff8241b05f2c143f4a4c473cb
sripwoud <me@sripwoud.xyz>
2024-08-05 12:54:58 +0200

    chore(ui): use `next/legacy/image`

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 import Link from 'next/link'
 import { ConnectButton } from './ConnectButton'
 import { HelpModal } from './HelpModal'
